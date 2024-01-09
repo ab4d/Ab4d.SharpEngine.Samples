a631e6f5e0a4e981c48e69b0b08d6559869f339f
Andrej Benedik <abenedik@ab4d.com>
2024-01-09 13:33:33 +0100

    Added StreamlinesSample

---
+++ Ab4d.SharpEngine.Samples.Common/AdvancedModels/StreamlinesSample.cs
@@ -0,0 +1,143 @@
+﻿using Ab4d.SharpEngine.Samples.Common.Utils;
+using System.Drawing;
+using System.Numerics;
+using Ab4d.SharpEngine.Common;
+using Ab4d.SharpEngine.Core;
+using Ab4d.SharpEngine.Materials;
+using Ab4d.SharpEngine.Meshes;
+using Ab4d.SharpEngine.SceneNodes;
+using Ab4d.SharpEngine.Utilities;
+using Ab4d.SharpEngine.Vulkan;
+
+namespace Ab4d.SharpEngine.Samples.Common.AdvancedModels;
+
+public class StreamlinesSample : CommonSample
+{
+    public override string Title => "Streamlines";
+    public override string Subtitle => "Streamlines with density encoded into color: blue: low density, red: high density\nStreamlines data are read from a csv file";
+
+    private StandardMaterial? _gradientMaterial;
+
+    public StreamlinesSample(ICommonSamplesContext context)
+        : base(context)
+    {
+    }
+
+    protected override void OnCreateScene(Scene scene)
+    {
+        if (scene.GpuDevice == null)
+            return;
+
+        // Create the gradient that will represent the density (blue: low density, red: high density)
+        var gradient = new GradientStop[]
+        {
+            new GradientStop(Colors.Blue, 0.0f),
+            new GradientStop(Colors.Aqua, 0.25f),
+            new GradientStop(Colors.LightGreen, 0.5f),
+            new GradientStop(Colors.Yellow, 0.75f),
+            new GradientStop(Colors.Red, 1.0f),
+        };
+
+        var gradientTexture = TextureFactory.CreateGradientTexture(scene.GpuDevice, gradient, textureWidth: 256);
+
+        _gradientMaterial = new StandardMaterial(gradientTexture);
+
+
+        // Sample data was created by using ParaView application and exporting the streamlines into csv file.
+        string sampleDataFileName = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources/Streamlines.csv");
+        sampleDataFileName = FileUtils.FixDirectorySeparator(sampleDataFileName);
+
+        ReadStreamlinesData(scene, sampleDataFileName);
+    }
+
+    protected override void OnDisposed()
+    {
+        if (_gradientMaterial != null)
+        {
+            _gradientMaterial.DisposeWithTexture();
+            _gradientMaterial = null;
+        }
+
+        base.OnDisposed();
+    }
+
+    private void ReadStreamlinesData(Scene scene, string sampleDataFileName)
+    {
+        // We will set the tube path position color based on the Density
+        string colorColumnName  = "Density"; // "AngularVelocity"
+        bool   invertColorValue = false;     // This needs to be set to true when using AngularVelocity
+
+
+        // Create csv file reader that can read data from a csv file
+        var csvDataReader = new CsvDataReader();
+        csvDataReader.ReadFile(sampleDataFileName);
+
+
+        csvDataReader.GetValuesRange(colorColumnName, out var minValue, out var maxValue);
+
+        float dataRange = maxValue - minValue;
+
+
+        var streamlineIndexes = csvDataReader.IndividualObjectIndexes;
+
+        if (streamlineIndexes == null)
+            return;
+
+        // Create the streamlines
+        var allStreamlineBounds = new BoundingBox();
+
+        for (var i = 0; i < streamlineIndexes.Length - 1; i++)
+        {
+            int startIndex = streamlineIndexes[i];
+            int endIndex = streamlineIndexes[i + 1] - 1;
+
+            int dataCount = endIndex - startIndex;
+
+            if (dataCount < 2) // Skip streamlines without any positions or with less than 2 positions
+                continue;
+
+            var positions = csvDataReader.GetPositions(startIndex, dataCount, out var bounds);
+
+            allStreamlineBounds.Add(bounds);
+
+            float[] dataValues = csvDataReader.GetValues(colorColumnName, startIndex, dataCount);
+
+            // Generate texture coordinates for each path position
+            // Because our texture is one dimensional gradient image (size 256 x 1)
+            // we set the x coordinate in range from 0 to 1 (0 = first gradient color; 1 = last gradient color).
+            
+            var positionsCount     = positions.Length;
+            var textureCoordinates = new Vector2[positionsCount];
+
+            for (int j = 0; j < dataCount; j++)
+            {
+                float relativeDataValue = (dataValues[j] - minValue) / dataRange;
+
+                if (invertColorValue)
+                    relativeDataValue = 1.0f - relativeDataValue;
+
+                textureCoordinates[j] = new Vector2(relativeDataValue, 0.5f);
+            }
+
+
+            var tubePathMesh = MeshFactory.CreateTubeMeshAlongPath(pathPositions: positions,
+                                                                   pathPositionTextureCoordinates: textureCoordinates,
+                                                                   radius: 0.03f,
+                                                                   isTubeClosed: true,
+                                                                   isPathClosed: false,
+                                                                   segments: 8);
+
+            var meshModelNode = new MeshModelNode(tubePathMesh, _gradientMaterial);
+
+            scene.RootNode.Add(meshModelNode);
+        }
+
+        if (targetPositionCamera != null)
+        {
+            targetPositionCamera.TargetPosition = allStreamlineBounds.GetCenterPosition();
+            targetPositionCamera.Distance = allStreamlineBounds.GetDiagonalLength();
+            targetPositionCamera.Heading = 30;
+            targetPositionCamera.Attitude = -20;
+        }
+    }
+}
---
+++ Ab4d.SharpEngine.Samples.Common/Utils/CsvDataReader.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using Ab4d.SharpEngine.Common;
+
+namespace Ab4d.SharpEngine.Samples.Common.Utils;
+
+/// <summary>
+/// CsvDataReader can read float data from a csv file that defines the column names in the first row.
+/// By default it is set up to read data from ParaView application.
+/// </summary>
+public class CsvDataReader
+{
+    public int DataCount { get; private set; }
+    public int ObjectsCount { get; private set; }
+
+    public string[]? ColumnNames { get; private set; }
+    public float[,]? DataArray { get; private set; }
+
+    // IndividualObjectIndexes is an array of indexes in DataArray at which individual objects (for example individual streamlines) start.
+    public int[]? IndividualObjectIndexes { get; private set; }
+
+    public string PositionXColumnName = "Points:0";
+    public string PositionYColumnName = "Points:1";
+    public string PositionZColumnName = "Points:2";
+
+
+    public void ReadFile(string fileName)
+    {
+        var fileLines = System.IO.File.ReadAllLines(fileName);
+
+        var columnSeparators = new char[] { ',', ';' };
+
+        var columnNames = fileLines[0].Split(columnSeparators, StringSplitOptions.RemoveEmptyEntries);
+        for (int i = 0; i < columnNames.Length; i++)
+            columnNames[i] = columnNames[i].Trim(' ', '"', '\''); // Trim leading spaces or quotes
+
+        var data = new float[fileLines.Length - 1, columnNames.Length];
+
+        for (var i = 1; i < fileLines.Length; i++)
+        {
+            var oneLine = fileLines[i].Split(columnSeparators, StringSplitOptions.RemoveEmptyEntries);
+            for (var j = 0; j < oneLine.Length; j++)
+                data[i - 1, j] = float.Parse(oneLine[j], NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture);
+        }
+
+        this.ColumnNames = columnNames;
+        this.DataArray = data;
+
+        this.DataCount = fileLines.Length - 1;
+
+        CollectIndividualObjectIndexes();
+
+        if (IndividualObjectIndexes != null)
+            this.ObjectsCount = IndividualObjectIndexes.Length;
+    }
+
+    public virtual void CollectIndividualObjectIndexes()
+    {
+        CheckIfFileRead();
+
+
+        // IntegrationTime always increases (or always decreases) in one line; when this direction is changed, then a new line is started.
+        // This method returns indexes at positions when a new line is started.
+
+        int integrationTimeIndex = Array.IndexOf(ColumnNames, "IntegrationTime");
+        if (integrationTimeIndex == -1)
+        {
+            return;
+            //throw new Exception("No IntegrationTime in the data. Please override the CollectIndividualObjectIndexes method to provide your own way to split the data between objects");
+        }
+
+
+        var indexes = new List<int>();
+        indexes.Add(0); // Add start of the first line
+
+        var data = this.DataArray;
+        int positionsCount = data.GetLength(0);
+
+        float i0 = data[0, integrationTimeIndex];
+        float i1 = data[1, integrationTimeIndex];
+
+        var globalDirectionSign = Math.Sign(i1 - i0);
+
+        i0 = i1;
+
+        for (int i = 2; i < positionsCount; i++)
+        {
+            i1 = data[i, integrationTimeIndex];
+
+            var oneStepDirectionSign = Math.Sign(i1 - i0);
+            i0 = i1;
+
+            if (oneStepDirectionSign != globalDirectionSign)
+                indexes.Add(i);
+        }
+
+        IndividualObjectIndexes = indexes.ToArray();
+    }
+
+    public float[] GetValues(string columnName)
+    {
+        return GetValues(columnName, out _, out _);
+    }
+
+    public float[] GetValues(string columnName, out float minValue, out float maxValue)
+    {
+        return GetValues(columnName, 0, this.DataCount, out minValue, out maxValue);
+    }
+
+    public float[] GetValues(string columnName, int startIndex, int valuesCount)
+    {
+        return GetValues(columnName, startIndex, valuesCount, out _, out _);
+    }
+
+    public float[] GetValues(string columnName, int startIndex, int valuesCount, out float minValue, out float maxValue)
+    {
+        CheckIfFileRead();
+
+        int index = Array.IndexOf(ColumnNames, columnName);
+        if (index == -1)
+            throw new Exception("No column with name " + columnName);
+
+        var data = this.DataArray;
+
+        minValue = data[startIndex, index];
+        maxValue = minValue;
+
+        
+        var values = new float[valuesCount];
+        values[0] = minValue;
+
+        int endPositionIndex = startIndex + valuesCount;
+        int positionsIndex   = 1;
+
+        for (int i = startIndex + 1; i < endPositionIndex; i++)
+        {
+            float oneValue = data[i, index];
+
+            values[positionsIndex] = oneValue;
+            positionsIndex++;
+
+            if (minValue > oneValue) minValue = oneValue;
+            if (maxValue < oneValue) maxValue = oneValue;
+        }
+
+        return values;
+    }
+
+    public void GetValuesRange(string columnName, out float minValue, out float maxValue)
+    {
+        CheckIfFileRead();
+
+        int index = Array.IndexOf(ColumnNames, columnName);
+        if (index == -1)
+            throw new Exception("No column with name " + columnName);
+
+        var data = this.DataArray;
+
+        minValue = data[0, index];
+        maxValue = minValue;
+
+        int dataCount = data.GetLength(0);
+
+        for (int i = 1; i < dataCount; i++)
+        {
+            float oneValue = data[i, index];
+
+            if (minValue > oneValue) minValue = oneValue;
+            if (maxValue < oneValue) maxValue = oneValue;
+        }
+    }
+    
+    public Vector3[] GetPositions()
+    {
+        BoundingBox bounds;
+        return GetPositions(out bounds);
+    }
+
+    public Vector3[] GetPositions(out BoundingBox bounds)
+    {
+        CheckIfFileRead();
+
+        int positionsCount = DataArray.GetLength(0);
+        return GetPositions(0, positionsCount, out bounds);
+    }
+
+    public Vector3[] GetPositions(int objectIndex, out BoundingBox bounds)
+    {
+        CheckIfFileRead();
+
+        int positionsCount = DataArray.GetLength(0);
+        return GetPositions(0, positionsCount, out bounds);
+    }
+
+    public Vector3[] GetPositions(int startIndex, int positionsCount, out BoundingBox bounds)
+    {
+        CheckIfFileRead();
+
+        var positions = new Vector3[positionsCount];
+
+        // Get indexes
+        int ix = Array.IndexOf(ColumnNames, PositionXColumnName);
+        int iy = Array.IndexOf(ColumnNames, PositionYColumnName);
+        int iz = Array.IndexOf(ColumnNames, PositionZColumnName);
+
+        if (ix == -1 || iy == -1 || iz == -1)
+            throw new FormatException("Cannot find position data columns");
+
+        var data = this.DataArray;
+
+        float x = data[startIndex, ix];
+        float y = data[startIndex, iy];
+        float z = data[startIndex, iz];
+
+        positions[0] = new Vector3(x, y, z);
+
+        float minX, maxX, minY, maxY, minZ, maxZ;
+
+        minX = maxX = x;
+        minY = maxY = y;
+        minZ = maxZ = z;
+
+
+        int endPositionIndex = startIndex + positionsCount;
+        int positionsIndex = 1;
+
+        for (int i = startIndex + 1; i < endPositionIndex; i++)
+        {
+            x = data[i, ix];
+            y = data[i, iy];
+            z = data[i, iz];
+
+            positions[positionsIndex] = new Vector3(x, y, z);
+            positionsIndex++;
+
+
+            if (minX > x) minX = x;
+            if (maxX < x) maxX = x;
+
+            if (minY > y) minY = y;
+            if (maxY < y) maxY = y;
+
+            if (minZ > z) minZ = z;
+            if (maxZ < z) maxZ = z;
+        }
+
+        bounds = new BoundingBox(new Vector3(minX, minY, minZ), new Vector3(maxX, maxY, maxZ));
+        return positions;
+    }
+
+    [MemberNotNull(nameof(ColumnNames))]
+    [MemberNotNull(nameof(DataArray))]
+    private void CheckIfFileRead()
+    {
+        if (ColumnNames == null || DataArray == null)
+            throw new Exception("File not yet read");
+    }
+}
