517724ec0916586d20d4261a15147485d1daed9a
Andrej Benedik <abenedik@ab4d.com>
2024-01-19 13:45:11 +0100

    Added AvaloniaMultiTouchSample

---
+++ Ab4d.SharpEngine.Samples.AvaloniaUI/Advanced/AvaloniaMultiTouchSample.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using Ab4d.SharpEngine.Cameras;
+using Ab4d.SharpEngine.Common;
+using Ab4d.SharpEngine.Materials;
+using Ab4d.SharpEngine.SceneNodes;
+using Ab4d.SharpEngine.Samples.AvaloniaUI.Common;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Ab4d.SharpEngine.Samples.AvaloniaUI.Advanced
+{
+    /// <summary>
+    /// Interaction logic for AvaloniaMultiTouchSample.xaml
+    /// </summary>
+    public partial class AvaloniaMultiTouchSample : UserControl
+    {
+        private GesturesCameraController? _mouseCameraController;
+
+        private TargetPositionCamera? _targetPositionCamera;
+
+        public AvaloniaMultiTouchSample()
+        {
+            InitializeComponent();
+
+            CreateTestScene();
+            SetupMouseCameraController();
+
+            this.Unloaded += (sender, args) => MainSceneView.Dispose();
+        }
+
+        private void SetupMouseCameraController()
+        {
+            _targetPositionCamera = new TargetPositionCamera()
+            {
+                Heading = -40,
+                Attitude = -30,
+                Distance = 500,
+                ViewWidth = 500,
+                TargetPosition = new Vector3(0, 0, 0),
+                ShowCameraLight = ShowCameraLightType.Always
+            };
+
+            MainSceneView.SceneView.Camera = _targetPositionCamera;
+
+            // Create GesturesCameraController that is defined in Common folder
+            _mouseCameraController = new GesturesCameraController(MainSceneView)
+            {
+                IsPinchGestureEnabled         = PinchGestureCheckBox.IsChecked ?? false,
+                IsScrollGestureEnabled        = ScrollGestureCheckBox.IsChecked ?? false,
+                RotateCameraWithScrollGesture = RotateWithScrollCheckBox.IsChecked ?? false,
+
+                RotateCameraConditions = MouseAndKeyboardConditions.LeftMouseButtonPressed,                                                   // this is already the default value but is still set up here for clarity
+                MoveCameraConditions = MouseAndKeyboardConditions.LeftMouseButtonPressed | MouseAndKeyboardConditions.ControlKey,             // this is already the default value but is still set up here for clarity
+                QuickZoomConditions = MouseAndKeyboardConditions.LeftMouseButtonPressed | MouseAndKeyboardConditions.RightMouseButtonPressed, // quick zoom is disabled by default
+                ZoomMode = CameraZoomMode.MousePosition,
+                RotateAroundMousePosition = true
+            };
+        }
+
+        private void CreateTestScene()
+        {
+            var planeModelNode = new PlaneModelNode(centerPosition: new Vector3(0, 0, 0), 
+                                                    size: new Vector2(400, 300), 
+                                                    normal: new Vector3(0, 1, 0), 
+                                                    heightDirection: new Vector3(0, 0, -1), 
+                                                    name: "BasePlane")
+            {
+                Material = StandardMaterials.Gray,
+                BackMaterial = StandardMaterials.Black
+            };
+
+            MainSceneView.Scene.RootNode.Add(planeModelNode);
+
+
+            var boxModel = new BoxModelNode(centerPosition: new Vector3(0, 20, 0), 
+                size: new Vector3(80, 40, 60), 
+                name: "Gold BoxModel")
+            {
+                Material = StandardMaterials.Gold,
+            };
+
+            MainSceneView.Scene.RootNode.Add(boxModel);
+        }
+
+        private void ScrollGestureCheckBox_OnIsCheckedChanged(object? sender, RoutedEventArgs e)
+        {
+            if (_mouseCameraController == null)
+                return;
+
+            _mouseCameraController.IsScrollGestureEnabled = ScrollGestureCheckBox.IsChecked ?? false;
+        }
+        
+        private void PinchGestureCheckBox_OnIsCheckedChanged(object? sender, RoutedEventArgs e)
+        {
+            if (_mouseCameraController == null)
+                return;
+
+            _mouseCameraController.IsPinchGestureEnabled = PinchGestureCheckBox.IsChecked ?? false;
+        }        
+
+        private void RotateWithScrollCheckBox_OnIsCheckedChanged(object? sender, RoutedEventArgs e)
+        {
+            if (_mouseCameraController == null)
+                return;
+
+            _mouseCameraController.RotateCameraWithScrollGesture = RotateWithScrollCheckBox.IsChecked ?? false;
+        }
+    }
+}
---
+++ Ab4d.SharpEngine.Samples.AvaloniaUI/Common/GesturesCameraController.cs
@@ -0,0 +1,137 @@
+﻿//#define WRITE_INFO_TO_OUTPUT
+
+using System;
+using System.Numerics;
+using Ab4d.SharpEngine.AvaloniaUI;
+using Ab4d.SharpEngine.Common;
+using Avalonia.Input;
+
+namespace Ab4d.SharpEngine.Samples.AvaloniaUI.Common;
+
+/// <summary>
+/// GesturesCameraController extends the standard MouseCameraController is processes Avalonia's mouse events for SharpEngine.
+/// This class adds support for Scroll and Pinch gestures.
+/// </summary>
+public class GesturesCameraController : MouseCameraController
+{
+    public bool IsPinchGestureEnabled { get; set; } = true;
+
+    public bool IsScrollGestureEnabled { get; set; } = true;
+
+    public bool RotateCameraWithScrollGesture { get; set; } = true;
+
+    private float _previousPinchScale = 1;
+    private Vector3? _lastPinchRotationCenterPosition;
+
+    public GesturesCameraController(SharpEngineSceneView sharpEngineSceneView, IInputElement? eventsSourceElement = null) : base(sharpEngineSceneView, eventsSourceElement)
+    {
+        Gestures.ScrollGestureEvent.AddClassHandler<SharpEngineSceneView>(ScrollGestureHandler);
+        Gestures.ScrollGestureEndedEvent.AddClassHandler<SharpEngineSceneView>(ScrollGestureEndedHandler);
+        Gestures.PinchEvent.AddClassHandler<SharpEngineSceneView>(PinchEventHandler);
+        Gestures.PinchEndedEvent.AddClassHandler<SharpEngineSceneView>(PinchEndedEventHandler);
+    }
+
+
+    private void ScrollGestureHandler(SharpEngineSceneView target, ScrollGestureEventArgs args)
+    {
+        if (!IsScrollGestureEnabled || !ReferenceEquals(target, this.SharpEngineSceneView))
+            return;
+
+
+        float mouseDx = (float)-args.Delta.X;
+        float mouseDy = (float)-args.Delta.Y;
+
+        if (RotateCameraWithScrollGesture)
+            RotateCamera(mouseDx, mouseDy);
+        else
+            MoveCamera(mouseDx, mouseDy);
+        
+        args.Handled = true;
+
+#if WRITE_INFO_TO_OUTPUT
+        System.Diagnostics.Debug.WriteLine($"Delta: {args.Delta}, ShouldEndScrollGesture: {args.ShouldEndScrollGesture}");
+#endif
+    }
+    
+    private void ScrollGestureEndedHandler(SharpEngineSceneView target, ScrollGestureEndedEventArgs args)
+    {
+        if (!IsScrollGestureEnabled || !ReferenceEquals(target, this.SharpEngineSceneView))
+            return;
+
+        EndMouseProcessing(); // This is required to prevent invalid camera rotation after next touch (this will not be needed anymore in the next version)
+
+        args.Handled = true;
+
+#if WRITE_INFO_TO_OUTPUT
+        System.Diagnostics.Debug.WriteLine("ScrollGestureEnded");
+#endif
+    }
+
+    private void PinchEventHandler(SharpEngineSceneView target, PinchEventArgs args)
+    {
+        if (!IsPinchGestureEnabled || !ReferenceEquals(target, this.SharpEngineSceneView))
+            return;
+
+        var mousePosition = new Vector2((float)args.ScaleOrigin.X, (float)args.ScaleOrigin.Y);
+        var scale = (float)args.Scale;
+
+
+        bool resetRotationCenterPosition = false;
+        Vector3? savedRotationCenterPosition = null;
+
+        // If ZoomMode is set to MousePosition and we have started a new zooming (there is more then 1 seconds from last zoom event),
+        // then we use hit testing to get new Camera's RotationCenterPosition
+        if (this.ZoomMode == CameraZoomMode.MousePosition)
+        {
+            // Save RotationCenterPosition so it can be reset at the end of this method
+            savedRotationCenterPosition = GetCameraRotationCenterPosition();
+            resetRotationCenterPosition = true;
+
+            if (lastZoomMousePosition != mousePosition)
+            {
+                bool isSupportedCamera = UpdateRotationCenterPosition(mousePosition, calculatePositionWhenNoObjectIsHit: true);
+                if (!isSupportedCamera)
+                    throw new NotSupportedException("MousePosition ZoomMode can be used only when MouseCameraController is controlling TargetPositionCamera or FreeCamera.");
+
+                lastZoomMousePosition            = mousePosition;
+                _lastPinchRotationCenterPosition = GetCameraRotationCenterPosition();
+            }
+            else
+            {
+                SetCameraRotationCenterPosition(_lastPinchRotationCenterPosition);
+            }
+        }
+
+
+        float scaleDiff = _previousPinchScale / scale;
+        _previousPinchScale = scale;
+
+        this.ChangeCameraDistance(scaleDiff, zoomToRotationCenterPosition: true);
+
+
+        // Set the RotationCenterPosition back to its previous value
+        if (resetRotationCenterPosition)
+            SetCameraRotationCenterPosition(savedRotationCenterPosition);
+
+#if WRITE_INFO_TO_OUTPUT
+        System.Diagnostics.Debug.WriteLine($"Scale: {args.Scale}, pos: {args.ScaleOrigin}");
+#endif
+
+        args.Handled = true;
+    }
+
+    private void PinchEndedEventHandler(SharpEngineSceneView target, PinchEndedEventArgs args)
+    {
+        if (!IsPinchGestureEnabled || !ReferenceEquals(target, this.SharpEngineSceneView))
+            return;
+
+        _previousPinchScale = 1;
+        lastZoomMousePosition = new Vector2(float.NaN, float.NaN);
+        
+        args.Handled = true;
+
+#if WRITE_INFO_TO_OUTPUT
+        System.Diagnostics.Debug.WriteLine("PinchEnded");
+#endif
+    }
+}
