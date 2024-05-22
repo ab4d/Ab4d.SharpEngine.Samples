5262c4b7c34b706ce2d50c77018a03f57bba5c17
Andrej Benedik <abenedik@ab4d.com>
2024-05-22 12:38:30 +0200

    Added ModelScalar

--- Ab4d.SharpEngine.Samples.Common/HitTesting/ModelScalarSample.cs
+++ Ab4d.SharpEngine.Samples.Common/HitTesting/ModelScalarSample.cs
@@ -4,58 +4,66 @@ using System.Numerics;
 using Ab4d.SharpEngine.Common;
 using Ab4d.SharpEngine.Materials;
 using Ab4d.SharpEngine.SceneNodes;
+using Ab4d.SharpEngine.Transformations;
 using Ab4d.SharpEngine.Utilities;
 using Ab4d.Vulkan;
 
 namespace Ab4d.SharpEngine.Samples.Common.HitTesting;
 
-public class ModelMoverSample : CommonSample
+public class ModelScalarSample : CommonSample
 {
-    public override string Title => "ModelMover sample";
-    public override string Subtitle => "Move selected sphere around. Click on other sphere to select it.\nRotate the camera with the right mouse button.";
+    public override string Title => "ModelScalar sample";
+    public override string Subtitle => "Scale the selected teapot. Click on other teapot to select it.\nRotate the camera with the right mouse button.";
 
-    private ModelMover? _modelMover;
+    private ModelScalar? _modelScalar;
 
     private readonly StandardMaterial _commonMaterial;
     private readonly StandardMaterial _selectedMaterial;
-    private readonly StandardMaterial _invalidPositionMaterial;
 
-    private SphereModelNode? _movingSphere;
-    private Vector3 _startCenterPosition;
-    private GroupNode? _testSpheresGroupNode;
+    private ModelNode? _scalingModel;
+    private GroupNode? _testModelsGroupNode;
     private PlanarShadowMeshCreator? _planarShadowMeshCreator;
     private MeshModelNode? _shadowModel;
+    private Vector3 _startScaleFactors;
 
-    private bool _preventCollisions = true;
-
-    public ModelMoverSample(ICommonSamplesContext context)
+    public ModelScalarSample(ICommonSamplesContext context)
         : base(context)
     {
         _commonMaterial = StandardMaterials.Silver;
         _selectedMaterial = StandardMaterials.Orange;
-        _invalidPositionMaterial = StandardMaterials.Red;
 
         RotateCameraConditions = MouseAndKeyboardConditions.RightMouseButtonPressed;
         MoveCameraConditions = MouseAndKeyboardConditions.RightMouseButtonPressed | MouseAndKeyboardConditions.ControlKey;
+        
+        RotateCameraConditions = MouseAndKeyboardConditions.Disabled;
+        MoveCameraConditions = MouseAndKeyboardConditions.Disabled;
 
         ShowCameraAxisPanel = true;
     }
 
     protected override void OnCreateScene(Scene scene)
     {
-        _testSpheresGroupNode = new GroupNode("TestSpheresGroup");
-        scene.RootNode.Add(_testSpheresGroupNode);
+        float teapotSize = 80;
+
+        var teapotMesh = TestScenes.GetTestMesh(TestScenes.StandardTestScenes.TeapotLowResolution, finalSize: new Vector3(teapotSize, teapotSize, teapotSize));
+
+        _testModelsGroupNode = new GroupNode("TestModelsGroup");
+        scene.RootNode.Add(_testModelsGroupNode);
 
         // Add 10 test spheres (we use while because some random positions may be already occupied by other spheres so we need more random positions)
-        while (_testSpheresGroupNode.Count < 10)
+        while (_testModelsGroupNode.Count < 10)
         {
             var randomPosition = GetRandomPosition(new Vector3(0, 75, 0), new Vector3(350, 100, 350));
 
-            if (!IsPositionFree(randomPosition, freeRadius: 50, _testSpheresGroupNode))
+            if (!IsPositionFree(randomPosition, freeRadius: teapotSize * 1.2f, _testModelsGroupNode))
                 continue; // Find another position
 
-            var sphereModelNode = new SphereModelNode(randomPosition, radius: 20, material: _commonMaterial);
-            _testSpheresGroupNode.Add(sphereModelNode);
+            var teapotModel = new MeshModelNode(teapotMesh, _commonMaterial, $"Teapot_{_testModelsGroupNode.Count}")
+            {
+                Transform = new StandardTransform(translateX: randomPosition.X, translateY: randomPosition.Y, translateZ: randomPosition.Z)
+            };
+
+            _testModelsGroupNode.Add(teapotModel);
         }
 
 
@@ -76,21 +84,20 @@ public class ModelMoverSample : CommonSample
         SetupPlanarShadow(scene);
     }
 
-    /// <inheritdoc />
     protected override void OnInputEventsManagerInitialized(ManualInputEventsManager inputEventsManager)
     {
-        if (_testSpheresGroupNode == null || Scene == null)
+        if (_testModelsGroupNode == null || Scene == null)
             return;
 
         // Use InputEventsManager to subscribe to click event on the spheres
-        var allTestModels = _testSpheresGroupNode.GetAllChildren<ModelNode>();
+        var allTestModels = _testModelsGroupNode.GetAllChildren<ModelNode>();
         var multiModelNodesEventsSource = new MultiModelNodesEventsSource(allTestModels);
 
         multiModelNodesEventsSource.PointerClick += (sender, args) =>
         {
             // start moving object on left click
             if (args.RayHitResult != null && args.PressedButtons == MouseButtons.Left)
-                StartMovingObject(args.RayHitResult.HitSceneNode);
+                StartScalingObject(args.RayHitResult.HitSceneNode);
         };
 
         inputEventsManager.RegisterEventsSource(multiModelNodesEventsSource);
@@ -106,46 +113,34 @@ public class ModelMoverSample : CommonSample
         // Create ModelMover
         // Note that ModelMover is not a SceneNode and cannot be added to the RootNote.
         // Instead, the models that are added to the scene are defined in _modelMover.ModelMoverGroupNode (see below).
-        _modelMover = new ModelMover(inputEventsManager);
+        _modelScalar = new ModelScalar(inputEventsManager);
         
         // To show ModelMover on top of other objects, se the CustomRenderingLayer to OverlayRenderingLayer.
-        _modelMover.CustomRenderingLayer = Scene.OverlayRenderingLayer;
+        _modelScalar.CustomRenderingLayer = Scene.OverlayRenderingLayer;
 
         // Handle ModelMoveStarted and ModelMoved
-        _modelMover.ModelMoveStarted += (sender, args) =>
+        _modelScalar.ModelScaleStarted += (sender, args) =>
         {
-            if (_movingSphere != null)
-                _startCenterPosition = _movingSphere.CenterPosition;
+            if (_scalingModel != null && _scalingModel.Transform is StandardTransform standardTransform)
+                _startScaleFactors = standardTransform.GetScaleFactors();
         };
 
-        _modelMover.ModelMoved += (sender, args) =>
+        _modelScalar.ModelScaled += (sender, args) =>
         {
-            if (_movingSphere == null)
+            if (_scalingModel == null)
                 return;
 
-            var newPosition = _startCenterPosition + args.MoveVector;
+            var newScaleFactors = args.ScaleFactors;
+            newScaleFactors *= _startScaleFactors;
 
-
-            // prevent moving sphere below plane with y = 0
-            if (newPosition.Y < 25)
-            {
-                args.PreventMove = true; // Set PreventMove to true to prevent automatic moving of ModelMover
+            // prevent scaling below 5% of the initial size
+            if (newScaleFactors.X < 0.05 || newScaleFactors.Y < 0.05 || newScaleFactors.Z < 0.05)
                 return;
-            }
 
-            
-            int sphereIndex = _testSpheresGroupNode.IndexOf(_movingSphere);
-            
-            // prevent collision
-            if (_preventCollisions && !IsPositionFree(newPosition, 40, _testSpheresGroupNode, skippedSphereIndex: sphereIndex))
-            {
-                _movingSphere.Material = _invalidPositionMaterial; // Show red material
-                args.PreventMove = true; // Set PreventMove to true to prevent automatic moving of ModelMover
-                return;
-            }
 
-            _movingSphere.Material = _selectedMaterial;
-            _movingSphere.CenterPosition = newPosition;
+            _scalingModel.Material = _selectedMaterial;
+            if (_scalingModel.Transform is StandardTransform standardTransform)
+                standardTransform.SetScale(newScaleFactors);
 
 
             if (_planarShadowMeshCreator != null && _shadowModel != null)
@@ -157,7 +152,7 @@ public class ModelMoverSample : CommonSample
             }
         };
 
-        _modelMover.ModelMoveEnded += (sender, args) =>
+        _modelScalar.ModelScaleEnded += (sender, args) =>
         {
             // Nothing to do here in this sample
         };
@@ -167,38 +162,38 @@ public class ModelMoverSample : CommonSample
         // !!! IMPORTAMT !!!
         // To show ModelMover we need to add ModelMoverGroupNode (as GroupNode) to the Scene.RootNode
 
-        Scene.RootNode.Add(_modelMover.ModelMoverGroupNode);
+        Scene.RootNode.Add(_modelScalar.ModelScalarGroupNode);
 
 
         // Start moving the first sphere
-        StartMovingObject(allTestModels[0]);
+        StartScalingObject(allTestModels[0]);
     }
 
-    private void StartMovingObject(SceneNode? sceneNode)
+    private void StartScalingObject(SceneNode? sceneNode)
     {
-        if (_modelMover == null)
+        if (_modelScalar == null)
             return;
 
-        if (_movingSphere != null)
+        if (_scalingModel != null)
         {
-            _movingSphere.Material = _commonMaterial;
-            _movingSphere = null;
+            _scalingModel.Material = _commonMaterial;
+            _scalingModel = null;
         }
 
-        var newMovingSphere = sceneNode as SphereModelNode;
+        var newScalingSphere = sceneNode as ModelNode;
 
-        if (newMovingSphere == null)
+        if (newScalingSphere == null)
         {
-            _modelMover.IsEnabled = false; // This will also hide _modelMover.ModelMoverGroupNode
+            _modelScalar.IsEnabled = false; // This will also hide _modelScalar.ModelScalarGroupNode
             return;
         }
 
-        newMovingSphere.Material = _selectedMaterial;
+        newScalingSphere.Material = _selectedMaterial;
 
-        _movingSphere = newMovingSphere;
+        _scalingModel = newScalingSphere;
 
-        _modelMover.Position = newMovingSphere.GetCenterPosition();
-        _modelMover.IsEnabled = true;
+        _modelScalar.Position = newScalingSphere.GetCenterPosition();
+        _modelScalar.IsEnabled = true;
     }
     
     private bool IsPositionFree(Vector3 spherePosition, float freeRadius, GroupNode groupNode, int skippedSphereIndex = -1)
@@ -210,8 +205,8 @@ public class ModelMoverSample : CommonSample
             if (i == skippedSphereIndex)
                 continue;
 
-            if (groupNode[i] is SphereModelNode oneSphere &&
-                (spherePosition - oneSphere.CenterPosition).Length() < freeRadius)
+            if (groupNode[i] is ModelNode oneModel &&
+                (spherePosition - oneModel.GetCenterPosition()).Length() < freeRadius)
             {
                 isFree = false; 
                 break;
@@ -223,20 +218,22 @@ public class ModelMoverSample : CommonSample
 
     private void SetupPlanarShadow(Scene scene)
     {
-        if (_testSpheresGroupNode == null)
+        if (_testModelsGroupNode == null)
             return;
 
         // Create PlanarShadowMeshCreator
-        _planarShadowMeshCreator = new PlanarShadowMeshCreator(_testSpheresGroupNode);
+        _planarShadowMeshCreator = new PlanarShadowMeshCreator(_testModelsGroupNode);
         _planarShadowMeshCreator.SetPlane(planeCenterPosition: new Vector3(0, 0, 0), planeNormal: new Vector3(0, 1, 0), planeHeightVector: new Vector3(0, 0, 1), planeSize: new Vector2(1000, 1000));
         _planarShadowMeshCreator.ClipToPlane = false; // No need to clip shadow to plane because plane is big enough (when having smaller plane, turn this on - this creates a lot of additional objects on GC)
-
+        _planarShadowMeshCreator.SimplifyNormalCalculation = false; // Because we show both front and back shadow material, we need to disable simplified normal calculation (see https://www.ab4d.com/help/SharpEngine/html/P_Ab4d_SharpEngine_Utilities_PlanarShadowMeshCreator_SimplifyNormalCalculation.htm)
+        
         _planarShadowMeshCreator.ApplyDirectionalLight(directionalLightDirection: new Vector3(0, -1, 0)); // Top down shadow
 
         if (_planarShadowMeshCreator.ShadowMesh != null)
         {
-            _shadowModel = new MeshModelNode(_planarShadowMeshCreator.ShadowMesh, StandardMaterials.DimGray, "PlanarShadowModel");
-            _shadowModel.Transform = new Ab4d.SharpEngine.Transformations.TranslateTransform(0, 0.05f, 0); // Lift the shadow 3D model slightly above the ground
+            _shadowModel = new MeshModelNode(_planarShadowMeshCreator.ShadowMesh, material: StandardMaterials.DimGray, "PlanarShadowModel");
+            _shadowModel.BackMaterial = _shadowModel.Material; // Set BackMaterial to prevent showing hole in the shadow that would show if only front-material would be used
+            _shadowModel.Transform = new TranslateTransform(0, 0.05f, 0); // Lift the shadow 3D model slightly above the ground
 
             scene.RootNode.Add(_shadowModel);
         }
@@ -245,7 +242,7 @@ public class ModelMoverSample : CommonSample
     /// <inheritdoc />
     protected override void OnDisposed()
     {
-        _modelMover?.Dispose(); // This will unsubscribe all pointer / mouse events from InputEventsManager
+        _modelScalar?.Dispose(); // This will unsubscribe all pointer / mouse events from InputEventsManager
         base.OnDisposed();
     }
 
@@ -253,16 +250,12 @@ public class ModelMoverSample : CommonSample
     {
         ui.CreateStackPanel(PositionTypes.Bottom | PositionTypes.Right);
 
-        ui.CreateCheckBox("Show X axis", _modelMover!.IsXAxisShown, isChecked => _modelMover!.IsXAxisShown = isChecked);
-        ui.CreateCheckBox("Show Y axis", _modelMover!.IsYAxisShown, isChecked => _modelMover!.IsYAxisShown = isChecked);
-        ui.CreateCheckBox("Show Z axis", _modelMover!.IsZAxisShown, isChecked => _modelMover!.IsZAxisShown = isChecked);
+        ui.CreateCheckBox("Show X axis", _modelScalar!.IsXAxisShown, isChecked => _modelScalar!.IsXAxisShown = isChecked);
+        ui.CreateCheckBox("Show Y axis", _modelScalar!.IsYAxisShown, isChecked => _modelScalar!.IsYAxisShown = isChecked);
+        ui.CreateCheckBox("Show Z axis", _modelScalar!.IsZAxisShown, isChecked => _modelScalar!.IsZAxisShown = isChecked);
+        
+        ui.AddSeparator();
 
-        ui.AddSeparator();
-        
-        ui.CreateCheckBox("Show movable planes", _modelMover!.ShowMovablePlanes, isChecked => _modelMover!.ShowMovablePlanes = isChecked);
-        
-        ui.AddSeparator();
-        
-        ui.CreateCheckBox("Prevent collisions", _preventCollisions, isChecked => _preventCollisions = isChecked);
+        ui.CreateCheckBox("Show center box", _modelScalar!.IsCenterBoxShown, isChecked => _modelScalar!.IsCenterBoxShown = isChecked);
     }
 }
