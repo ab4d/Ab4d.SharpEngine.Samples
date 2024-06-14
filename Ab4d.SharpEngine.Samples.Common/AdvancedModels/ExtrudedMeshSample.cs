73d49334ec3a7e43ba78010a14ed6278b0f11953
Andrej Benedik <abenedik@ab4d.com>
2024-06-14 17:40:03 +0200

    Work on extruded sample

--- Ab4d.SharpEngine.Samples.Common/AdvancedModels/ExtrudedMeshSample.cs
+++ Ab4d.SharpEngine.Samples.Common/AdvancedModels/ExtrudedMeshSample.cs
@@ -93,32 +93,20 @@ public class ExtrudedMeshSample : CommonSample
         // Get 2D shape positions
         var shape2DPositions = GetSimpleShapePositions(currentShapeType);
 
+        bool isShapeYUp = false; // The 2D coordinates that defined the shape have Y axis down: (0,0) is at top left corner
+
+
+        
+        // Triangulate 2D shape
+        //var triangleIndices = Triangulator.Triangulate(shape2DPositions).ToArray();
+
+        Triangulator triangulator = new Triangulator(shape2DPositions, isYAxisUp: isShapeYUp);
+        var triangleIndices =  triangulator.CreateTriangleIndices().ToArray();
+
+
         // Convert 2D positions to 3D positions (set y to zero)
-
-        // IMPORTANT:
-        // In this sample the shape2DPositions are oriented in anti-clockwise orientation.
-        // If we map: x2 = x1, y2 = 0, z2 = y1 this changes the order of 3D position to clockwise.
-        // The reason for this is that by default the Z axis is pointing to the viewer
-        // (but if 2D coordinate system is put to the XY plane, then Y is pointing into the screen).
-        // To prevent flipping the orientation, we need to negate the 2D y value when it is set to 3D z position.
-        //
-        // The code below shows how to correctly convert 2D positions to 3D position for all 4 possible coordinate systems.
-        //
-        // This can be tested by unchecking the "Show individual triangles" checkbox.
-        // In this case the front triangles are shown with orange material and back triangles are shown with red material.
-
-        float yInvertFactor = 1;
-
-        if (Scene != null)
-        {
-            var coordinateSystem = Scene.GetCoordinateSystem();
-            if (coordinateSystem == CoordinateSystems.YUpLeftHanded || coordinateSystem == CoordinateSystems.ZUpRightHanded)
-                yInvertFactor = -1;
-        }
-
-        var shape3DPositions = new Vector3[shape2DPositions.Length];
-        for (var i = 0; i < shape2DPositions.Length; i++)
-            shape3DPositions[i] = new Vector3(shape2DPositions[i].X, 0, shape2DPositions[i].Y * yInvertFactor);
+        // See comments in Convert2DTo3D method for more info
+        var shape3DPositions = Convert2DTo3D(shape2DPositions, Scene, triangulator.IsClockwise);
 
 
         // Show shape as a poly-line
@@ -127,9 +115,6 @@ public class ExtrudedMeshSample : CommonSample
         polyLineNode.Transform = new TranslateTransform(y: 80);
         parentGroupNode.Add(polyLineNode);
 
-
-        // Triangulate 2D shape
-        var triangleIndices = Triangulator.Triangulate(shape2DPositions).ToArray();
 
         if (_showIndividualTriangles)
         {
@@ -173,77 +158,40 @@ public class ExtrudedMeshSample : CommonSample
     {
         // Shape with holes is represented by multiple arrays of positions
         // The holes are identified by the orientation of positions (clockwise or anti-clockwise) that is different from the orientation of positions in the outer polygons. 
+        // In this sample the outer polygons are oriented in clockwise direction, holes in anti-clockwise direction
         Vector2[][] multiplePolygons = GetMultiShapePositions(currentShapeType);
 
-        bool isShapeYUp = false; // The 2D coordinates that defined the shape have Y axis down (0,0) is at top left corner
+        bool isShapeYUp = false; // The 2D coordinates that defined the shape have Y axis down: (0,0) is at top left corner
 
         var outerPolygon = multiplePolygons[0];
         var triangulator = new Triangulator(outerPolygon, isYAxisUp: isShapeYUp);
 
         for (int i = 1; i < multiplePolygons.Length; i++)
         {
+            // When calling AddHole the method checks if the specified positions are oriented in the opposite direction as the outerPolygon.
+            // If not, then the isCorrectOrientation is set to false and we can fix that by reversing the orientation of positions.
             bool isCorrectOrientation = triangulator.AddHole(multiplePolygons[i]);
             if (!isCorrectOrientation)
                 ReversePointsOrder(multiplePolygons[i]);
         }
         
+        // After we added the outer polygon and all holes, we can call the Triangulate method.
         triangulator.Triangulate(out List<Vector2> allPolygonPositions, out List<int> triangleIndicesList);
 
-        // Triangulate 2D shapes
-        // Triangulator also generates a list of all polygon positions
+
+        // The same can be achieved by calling the static Triangulator.Triangulate method.
+        // But here we need to make sure that the hole polygons are correctly oriented as we cannot switch their order.
         //Triangulator.Triangulate(multiplePolygons, out List<Vector2> allPolygonPositions, out List<int> triangleIndicesList);
 
 
         // Convert 2D positions to 3D positions (set y to zero)
+        // See comments in Convert2DTo3D method for more info
+        var shape3DPositions = Convert2DTo3D(allPolygonPositions.ToArray(), Scene, triangulator.IsClockwise);
 
-        // IMPORTANT:
-        // In this sample the shape2DPositions are oriented in anti-clockwise orientation.
-        // If we map: x2 = x1, y2 = 0, z2 = y1 this changes the order of 3D positions to clockwise.
-        // The reason for this is that by default the Z axis is pointing to the viewer
-        // (but if 2D coordinate system is put to the XY plane, then Z is pointing into the screen).
-        // To prevent flipping the orientation, we need to negate the 2D y value when it is set to 3D z position.
-        //
-        // The code below shows how to correctly convert 2D positions to 3D position for all 4 possible coordinate systems.
-        //
-        // This can be tested by unchecking the "Show individual triangles" checkbox.
-        // In this case the front triangles are shown with orange material and back triangles are shown with red material.
-
-        float yInvertFactor = 1;
-
-        if (Scene != null)
-        {
-            var coordinateSystem = Scene.GetCoordinateSystem();
-            if (coordinateSystem == CoordinateSystems.YUpLeftHanded || coordinateSystem == CoordinateSystems.ZUpRightHanded)
-                yInvertFactor = -1;
-        }
-
-        if (isShapeYUp)
-            yInvertFactor *= -1; // flip yInvertFactor
-
-
-        var shape3DPositions = new Vector3[allPolygonPositions.Count];
-        for (var i = 0; i < allPolygonPositions.Count; i++)
-            shape3DPositions[i] = new Vector3(allPolygonPositions[i].X, 0, allPolygonPositions[i].Y * yInvertFactor);
-
-
-        //// Convert 2D shape to 3D positions
-        //var shape3DPositions = new Vector3[allPolygonPositions.Count];
-        //for (var i = 0; i < allPolygonPositions.Count; i++)
-        //    shape3DPositions[i] = new Vector3(allPolygonPositions[i].X, 0, allPolygonPositions[i].Y); // Set y to zero
-
-
-        // Show shape as a poly-line
-        //var polyLineNode = new PolyLineNode(shape3DPositions, lineColor: Colors.Orange, lineThickness: 3);
-        //polyLineNode.IsClosed = true; // Connect last and first position to close the poly-line
-        //polyLineNode.Transform = new TranslateTransform(y: 80);
-        //parentGroupNode.Add(polyLineNode);
 
         for (var i = 0; i < multiplePolygons.Length; i++)
         {
-            var onePolygon = multiplePolygons[i];
-            var onePolygonPositions3D = new Vector3[onePolygon.Length];
-            for (var j = 0; j < onePolygon.Length; j++)
-                onePolygonPositions3D[j] = new Vector3(onePolygon[j].X, 0, onePolygon[j].Y * yInvertFactor);
+            var onePolygonPositions3D = Convert2DTo3D(multiplePolygons[i], Scene, triangulator.IsClockwise);
 
             // Show shape as a poly-line
             var polyLineNode = new PolyLineNode(onePolygonPositions3D, lineColor: Colors.Orange, lineThickness: 3);
@@ -288,15 +236,12 @@ public class ExtrudedMeshSample : CommonSample
             triangleIndices: triangleIndices,
             allPolygons: multiplePolygons,
             isSmooth: false,
-            isYAxisUp: triangulator.IsYAxisUp,
             flipNormals: flipNormals,
-            //modelOffset: new Vector3(0, 0, 0),
             modelOffset: new Vector3(0, -130, 0),
             extrudeVector: _extrudeVector,
             shapeYVector: _shapeYVector, // A 3D vector that defines the 3D direction along the 2D shape surface (i.e., the Y axis of the base 2D shape)
-            //extrudeVector: new Vector3(0, 0, 30),
-            //shapeYVector: new Vector3(0, 1, 0), 
             textureCoordinatesGenerationType: MeshFactory.ExtrudeTextureCoordinatesGenerationType.None,
+            isYAxisUp: false,
             closeBottom: true,
             closeTop: true);
 
@@ -312,6 +257,45 @@ public class ExtrudedMeshSample : CommonSample
 
 
         parentGroupNode.Add(extrudedModelNode);
+    }
+
+    private Vector3[] Convert2DTo3D(Vector2[] shape2DPositions, Scene? scene, bool isShapeClockwise)
+    {
+        // Convert 2D positions to 3D positions (set y to zero)
+
+        // We are converting 2D XY coordinates to 3D coordinates to XZ plane (by adding y = 0).
+        // Because in this sample in 2D coordinates the Y axis is pointing down ((0,0) is at top left corner),
+        // we can directly copy that to 3D coordinates when we have YUpRightHanded (default) or ZUpLeftHanded coordinate system.
+        // If we have YUpLeftHanded or ZUpRightHanded, then Z axis is oriented in the other direction
+        // and in this case we need to negate the y coordinate.
+        //
+        // This can be tested by unchecking the "Show individual triangles" checkbox.
+        // In this case the front triangles are shown with orange material and back triangles are shown with red material.
+        
+        float yInvertFactor = 1;
+
+        if (scene != null)
+        {
+            var coordinateSystem = scene.GetCoordinateSystem();
+            if (coordinateSystem == CoordinateSystems.YUpLeftHanded || coordinateSystem == CoordinateSystems.ZUpRightHanded)
+                yInvertFactor = -1;
+        }
+
+        // To show the top of the 2D shape with front material (set to Material property),
+        // the positions in the shape need to be oriented in the clock-wise direction.
+        // If they are not, we flip y. This also flips the orientation of the triangles.
+        // To demonstrate that, set the isAntiClockwise property in GetSimpleShapePositions method to false.
+
+        if (!isShapeClockwise)
+            yInvertFactor *= -1; // flip yInvertFactor
+
+
+        var shape3DPositions = new Vector3[shape2DPositions.Length];
+
+        for (var i = 0; i < shape2DPositions.Length; i++)
+            shape3DPositions[i] = new Vector3(shape2DPositions[i].X, 0, shape2DPositions[i].Y * yInvertFactor);
+
+        return shape3DPositions;
     }
 
     private GroupNode CreateIndividualTrianglesNode(Vector3[] shape3DPositions, int[] triangleIndices)
@@ -373,28 +357,30 @@ public class ExtrudedMeshSample : CommonSample
                 shapePositions = CreateNGonShape(center: new Vector2(0, 0), 
                                                  radius: 50, 
                                                  numCorners: 6, 
-                                                 isAntiClockwise: true);
+                                                 isClockwiseOrientation: true,
+                                                 isYAxisUp: false);
                 break;
 
             case SampleShapeTypes.Circle:
                 shapePositions = CreateNGonShape(center: new Vector2(0, 0), 
                                                  radius: 50, 
                                                  numCorners: 60, 
-                                                 isAntiClockwise: true);
+                                                 isClockwiseOrientation: false,
+                                                 isYAxisUp: false);
                 break;
             
             case SampleShapeTypes.CShape:
-                // anti-clockwise orientation of C shape
+                // Clockwise orientation of C shape
                 shapePositions = new Vector2[]
                 {
-                    new Vector2(60, -40), 
-                    new Vector2(-40, -40), 
-                    new Vector2(-40, 40), 
-                    new Vector2(40, 40), 
-                    new Vector2(40, 20), 
-                    new Vector2(-20, 20), 
+                    new Vector2(40, -20),
                     new Vector2(-20, -20), 
-                    new Vector2(60, -20)
+                    new Vector2(-20, 20), 
+                    new Vector2(60, 20), 
+                    new Vector2(60, 40), 
+                    new Vector2(-40, 40), 
+                    new Vector2(-40, -40), 
+                    new Vector2(40, -40)
                 };
                 break;
 
@@ -451,17 +437,20 @@ public class ExtrudedMeshSample : CommonSample
                 var rectangleShapePositions = CreateNGonShape(center: new Vector2(0, 0), 
                                                               radius: 50, 
                                                               numCorners: 4, 
-                                                              isAntiClockwise: true);
+                                                              isClockwiseOrientation: true,
+                                                              isYAxisUp: false);
 
-                var hole1 = CreateNGonShape(center: new Vector2(5, 12), 
+                var hole1 = CreateNGonShape(center: new Vector2(0, 22), 
                                             radius: 6, 
                                             numCorners: 10, 
-                                            isAntiClockwise: false);
+                                            isClockwiseOrientation: false,
+                                            isYAxisUp: false);
 
-                var hole2 = CreateNGonShape(center: new Vector2(20, -5),
+                var hole2 = CreateNGonShape(center: new Vector2(15, 5),
                                             radius: 10, 
                                             numCorners: 20, 
-                                            isAntiClockwise: false);
+                                            isClockwiseOrientation: false,
+                                            isYAxisUp: false);
 
                 multiShapePositions = new Vector2[3][] { rectangleShapePositions, hole1, hole2 };
                 break;
@@ -473,16 +462,20 @@ public class ExtrudedMeshSample : CommonSample
         return multiShapePositions;
     }
 
-    private static Vector2[] CreateNGonShape(Vector2 center, float radius, int numCorners, bool isAntiClockwise = true)
+    private static Vector2[] CreateNGonShape(Vector2 center, float radius, int numCorners, bool isClockwiseOrientation = true, bool isYAxisUp = false)
     {
         var corners = new Vector2[numCorners];
         var angleStep = 2 * MathF.PI / numCorners;
+
+        // When the direction of Y axis is changed, then the orientation is also changed
+        if (isYAxisUp)
+            isClockwiseOrientation = !isClockwiseOrientation;
 
         for (var i = 0; i < numCorners; i++)
         {
             var (sin, cos) = MathF.SinCos(i * angleStep);
 
-            if (!isAntiClockwise)
+            if (!isClockwiseOrientation)
                 cos = -cos; // this inverses the orientation of a triangle
 
             corners[i] = new Vector2(center.X + cos * radius, center.Y + sin * radius);
@@ -532,7 +525,7 @@ public class ExtrudedMeshSample : CommonSample
         ui.AddSeparator();
 
         var extrudeVectors = new Vector3[] { new Vector3(0, 15, 0), new Vector3(0, 30, 0), new Vector3(30, 0, 0), new Vector3(0, 0, -30), new Vector3(10, 30, 0) };
-        var shapeYVectors = new Vector3[]  { new Vector3(0, 0, -1), new Vector3(0, 0, -1), new Vector3(0, 1, 0),  new Vector3(0, 1, 0),  new Vector3(0, 0, -1) };
+        var shapeYVectors = new Vector3[]  { new Vector3(0, 0, 1), new Vector3(0, 0, 1), new Vector3(0, -1, 0),  new Vector3(0, -1, 0),  new Vector3(0, 0, 1) }; // note that shape 2D y axis down
         ui.CreateComboBox(extrudeVectors.Select(v => $"({v.X:F0}, {v.Y:F0}, {v.Z:F0})").ToArray(), (itemIndex, itemText) =>
         {
             _extrudeVector = extrudeVectors[itemIndex];
