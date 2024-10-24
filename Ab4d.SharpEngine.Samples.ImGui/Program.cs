802566658621a67a1bfc79a9465a135f9bf39e8c
Rok Mandeljc <rok.mandeljc@gmail.com>
2024-10-24 11:24:32 +0200

    ImGui sample: refactor setup code
    Inline the Silk window callbacks. Separate the setup code into
    smaller functions to make them easier to read.

--- Ab4d.SharpEngine.Samples.ImGui/Program.cs
+++ Ab4d.SharpEngine.Samples.ImGui/Program.cs
@@ -34,17 +34,54 @@ internal class Program
 
     private static void Main(string[] args)
     {
+        // Ab4d.SharpEngine Samples License can be used only for Ab4d.SharpEngine samples.
+        // To use Ab4d.SharpEngine in your project, get a license from ab4d.com/trial or ab4d.com/purchase
+        Ab4d.SharpEngine.Licensing.SetLicense(licenseOwner: "AB4D",
+            licenseType: "SamplesLicense",
+            platforms: "All",
+            license: "5B53-8A17-DAEB-5B03-3B90-DD5B-958B-CA4D-0B88-CE79-FBB4-6002-D9C9-19C2-AFF8-1662-B2B2");
+
+        // Create window using Silk; setup is performed once window is created/loaded.
         var options = WindowOptions.DefaultVulkan;
         options.Size = new Vector2D<int>(800, 600);
         options.Title = "Silk.NET + SharpEngine + ImGui";
 
         _window = Window.Create(options);
 
-        _window.Load += OnLoad;
-        _window.Render += OnRender;
-        _window.Update += OnUpdate;
-        _window.FramebufferResize += OnFramebufferResize;
-        _window.Closing += OnClose;
+        _window.Load += () =>
+        {
+            SetupInput();
+            SetupSharpEngine();
+            CreateScene();
+
+            // Add ImGui renderer to the scene view
+            Debug.Assert(_sceneView != null, nameof(_sceneView) + " != null");
+            _imGuiRenderer = new ImGuiRenderer(_sceneView, () =>
+            {
+                if (ShowOtherWindow)
+                {
+                    ImGuiNET.ImGui.Begin("Another window", ref ShowOtherWindow);
+                    ImGuiNET.ImGui.Text("Some text");
+                    ImGuiNET.ImGui.End();
+                }
+
+                if (ShowDemoWindow)
+                {
+                    ImGuiNET.ImGui.ShowDemoWindow(ref ShowDemoWindow);
+                }
+
+                return true; // Always update
+            });
+        };
+
+        _window.Render += (double obj) =>
+        {
+            _sceneView?.Render();
+        };
+        _window.FramebufferResize += (Vector2D<int> size) =>
+        {
+            _sceneView?.Resize(renderNextFrameAfterResize: true);
+        };
 
         _window.Initialize();
 
@@ -52,7 +89,8 @@ internal class Program
 
         _window.Dispose();
     }
-    static unsafe void OnLoad()
+
+    private static void SetupInput()
     {
         // Register input events on fist available keyboard and mouse
         Debug.Assert(_window != null, nameof(_window) + " != null");
@@ -74,13 +112,17 @@ internal class Program
             _mouse.MouseMove += OnMouseMove;
             _mouse.Scroll += OnMouseScroll;
         }
+    }
 
-        // Set up SharpEngine
+    private static unsafe void SetupSharpEngine()
+    {
+        // Setup SharpEngine to use Silk window as presentation surface
         _vulkanSurfaceProvider = new CustomVulkanSurfaceProvider(instance =>
             {
+                Debug.Assert(_window != null, nameof(_window) + " != null");
                 Debug.Assert(_window.VkSurface != null, "_window.VkSurface != null");
-                var vkNonDispatchableHandle = _window.VkSurface.Create(new VkHandle(instance.Handle), (byte*)null);
-                return new SurfaceKHR(vkNonDispatchableHandle.Handle);
+                var surfaceHandle = _window.VkSurface.Create(new VkHandle(instance.Handle), (byte*)null);
+                return new SurfaceKHR(surfaceHandle.Handle);
             },
             addDefaultSurfaceExtensions: true);
 
@@ -90,8 +132,12 @@ internal class Program
         };
 
         _vulkanDevice = VulkanDevice.Create(defaultSurfaceProvider: _vulkanSurfaceProvider, engineCreateOptions);
+    }
 
+    private static void CreateScene()
+    {
         // Set up test scene
+        Debug.Assert(_vulkanDevice != null, nameof(_vulkanDevice) + " != null");
         _scene = new Scene(_vulkanDevice);
 
         // Add lights
@@ -111,6 +157,7 @@ internal class Program
         // Create scene view
         _sceneView = new SceneView(_scene);
         _sceneView.BackgroundColor = Color4.White;
+        Debug.Assert(_vulkanSurfaceProvider != null, nameof(_vulkanSurfaceProvider) + " != null");
         _sceneView.Initialize(_vulkanSurfaceProvider, dpiScaleX: 1.0f, dpiScaleY: 1.0f);
         
         // Camera
@@ -133,43 +180,6 @@ internal class Program
             RotateAroundPointerPosition = false,
             ZoomMode = CameraZoomMode.PointerPosition,
         };
-
-        // Add ImGui renderer to the scene view
-        _imGuiRenderer = new ImGuiRenderer(_sceneView, () =>
-        {
-            if (ShowOtherWindow)
-            {
-                ImGuiNET.ImGui.Begin("Another window", ref ShowOtherWindow);
-                ImGuiNET.ImGui.Text("Some text");
-                ImGuiNET.ImGui.End();
-            }
-
-            if (ShowDemoWindow)
-            {
-                ImGuiNET.ImGui.ShowDemoWindow(ref ShowDemoWindow);
-            }
-
-            return true; // Always update
-        });
-    }
-
-    private static void OnRender(double obj)
-    {
-        if (_scene != null && _sceneView != null)
-            _sceneView.Render();
-    }
-
-    private static void OnUpdate(double obj)
-    {
-    }
-
-    private static void OnFramebufferResize(Vector2D<int> newSize)
-    {
-        _sceneView?.Resize(renderNextFrameAfterResize: true);
-    }
-
-    private static void OnClose()
-    {
     }
 
     #region Input
