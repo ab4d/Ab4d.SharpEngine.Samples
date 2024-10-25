172bcc29cb5adba09dbc926a5db688e04b3ad96c
Andrej Benedik <abenedik@ab4d.com>
2024-10-25 09:47:27 +0200

    Fixed using centerPosition in MeshFactory.CreateHashSymbolMesh

--- Ab4d.SharpEngine.Samples.Common/Animations/SharpEngineLogoAnimation.cs
+++ Ab4d.SharpEngine.Samples.Common/Animations/SharpEngineLogoAnimation.cs
@@ -61,7 +61,7 @@ public class SharpEngineLogoAnimation : IDisposable
 
 
         // Create hash symbol
-        var hashSymbolMesh = MeshFactory.CreateHashSymbolMesh(centerPosition: new Vector3(0, HashModelBarThickness * -0.5f, 0),
+        var hashSymbolMesh = MeshFactory.CreateHashSymbolMesh(centerPosition: new Vector3(0, 0, 0),
                                                               shapeYVector: new Vector3(0, 0, 1),
                                                               extrudeVector: new Vector3(0, HashModelBarThickness, 0),
                                                               size: HashModelSize,
