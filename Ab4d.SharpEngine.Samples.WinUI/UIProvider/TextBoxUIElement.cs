c3fca0b0efb50de7da26afea29ce93b685534eef
Andrej Benedik <abenedik@ab4d.com>
2025-01-28 11:29:35 +0100

    Fixed WinUI multiline TextBox

--- Ab4d.SharpEngine.Samples.WinUI/UIProvider/TextBoxUIElement.cs
+++ Ab4d.SharpEngine.Samples.WinUI/UIProvider/TextBoxUIElement.cs
@@ -9,6 +9,8 @@ public class TextBoxUIElement : WinUIElement
     private TextBox _textBox;
     private Action<string>? _textChangedAction;
 
+    private bool _fixNewLine = true;
+
     public TextBoxUIElement(WinUIProvider winUIProvider, float width, float height, string? initialText, Action<string>? textChangedAction)
         : base(winUIProvider)
     {
@@ -16,7 +18,6 @@ public class TextBoxUIElement : WinUIElement
         {
             Text = initialText,
             FontSize = winUIProvider.FontSize,
-            AcceptsReturn = true,
         };
 
         if (width > 0)
@@ -26,7 +27,19 @@ public class TextBoxUIElement : WinUIElement
             _textBox.Height = height;
 
         if (height > 0 || (initialText != null && initialText.Contains('\n'))) // is multiline?
+        {
             _textBox.AcceptsReturn = true;
+            _textBox.Text = initialText;
+
+            // UH UH:
+            // TextBox in WinUI just removes the '\n' from the text and uses '\r' instead.
+            // Maybe this is fixed in the future - check here is '\n' is preserved and in this case do not replace '\r' by '\n' in the TextBoxOnTextChanged.
+            // See also:
+            // https://stackoverflow.com/questions/42867242/uwp-textbox-puts-r-only-how-to-set-linebreak
+            // https://stackoverflow.com/questions/35138047/uwp-textbox-selectedtext-changes-r-n-to-r
+            if (initialText != null && initialText.Contains('\n') && _textBox.Text != null && _textBox.Text.Contains('\n'))
+                _fixNewLine = false;
+        }
 
 
         if (textChangedAction != null)
@@ -40,7 +53,12 @@ public class TextBoxUIElement : WinUIElement
 
     private void TextBoxOnTextChanged(object? sender, TextChangedEventArgs e)
     {
-        _textChangedAction?.Invoke(_textBox.Text ?? "");
+        var text = _textBox.Text ?? "";
+
+        if (_fixNewLine)
+            text = text.Replace('\r', '\n'); // see comment in constructor
+
+        _textChangedAction?.Invoke(text);
     }
 
 
