4dadea434d6967bbfa99296d2006e22c1bc762fb
Andrej Benedik <abenedik@ab4d.com>
2025-02-04 17:30:28 +0100

    Updated CustomOverlayPanelSample

--- Ab4d.SharpEngine.Samples.Common/Advanced/CustomOverlayPanelSample.cs
+++ Ab4d.SharpEngine.Samples.Common/Advanced/CustomOverlayPanelSample.cs
@@ -15,6 +15,8 @@ public sealed class CustomOverlayPanelSample : CommonSample
     public override string Title { get; } = "Custom Overlay Panel";
     public override string Subtitle { get; } = "This samples shows how to render a custom scene to a texture that is shown as a sprite on top of another 3D scene.";
 
+    private readonly bool RenderedToSharedTexture = true; // When true, then the GpuImage stays in the GPU memory - this is much faster
+
     private readonly int _overlayPanelWidth = 256;
     private readonly int _overlayPanelHeight = 256;
      
@@ -22,9 +24,9 @@ public sealed class CustomOverlayPanelSample : CommonSample
      
     private Scene? _overlayPanelScene;
     private SceneView? _overlayPanelSceneView;
-     
+
+    private GpuImage? _overlayPanelGpuImage;
     private RawImageData? _rawImageData;
-    private GpuImage? _gpuImage;
 
     public CustomOverlayPanelSample(ICommonSamplesContext context) 
         : base(context)
@@ -110,20 +112,6 @@ public sealed class CustomOverlayPanelSample : CommonSample
         _overlayPanelScene.RootNode.Add(boxModel);
 
 
-        // Create RawImageData and GpuImage that will be used to show the rendered SceneView as a sprite (see _parentSceneView_SceneUpdating)
-        _rawImageData = new RawImageData(width: _overlayPanelWidth, 
-                                         height: _overlayPanelHeight,
-                                         stride: 4 * _overlayPanelWidth,
-                                         format: Format.R8G8B8A8Unorm,
-                                         data: new byte[4 * _overlayPanelWidth * _overlayPanelHeight],
-                                         checkTransparency: false)
-        {
-            HasTransparentPixels = true
-        };
-
-        _gpuImage = new GpuImage(gpuDevice, _rawImageData);
-
-
         // Create SpriteBatch
         _spriteBatch = parentSceneView.CreateOverlaySpriteBatch("OverlaySceneViewSpriteBatch");
 
@@ -134,24 +122,78 @@ public sealed class CustomOverlayPanelSample : CommonSample
     
     private void ParentSceneView_SceneUpdating(object? o, EventArgs e)
     {
-        if (_overlayPanelSceneView == null || _gpuImage == null || _spriteBatch == null || _rawImageData == null)
+        if (GpuDevice == null || _overlayPanelSceneView == null || _overlayPanelSceneView.SwapChain == null || _overlayPanelSceneView.RenderingContext == null || _spriteBatch == null)
             return;
 
-        // Render Overlay 3D scene to _rawImageData
-        // NOTE that this copies the rendered texture to CPU memory.
-        // In the next version it will be possible to render to a GpuImage that would stay in GPU memory.
-        _overlayPanelSceneView.RenderToRawImageData(_rawImageData);
+        bool isNewFrameRendered = _overlayPanelSceneView.Render(forceRender: false);
+        
+        if (!isNewFrameRendered)
+            return;
 
-        // Copy the _rawImageData to a GpuImage that is rendered as a sprite
-        _gpuImage.CopyDataToImage(_rawImageData.Data);
+        if (_overlayPanelGpuImage != null && (_overlayPanelGpuImage.Width != _overlayPanelSceneView.Width || _overlayPanelGpuImage.Height != _overlayPanelSceneView.Height))
+        {
+            _overlayPanelGpuImage.Dispose();
+            _overlayPanelGpuImage = null;
+        }
 
+        if (_overlayPanelGpuImage == null)
+        {
+            _overlayPanelGpuImage = new GpuImage(GpuDevice,
+                                                 _overlayPanelSceneView.Width, _overlayPanelSceneView.Height, _overlayPanelSceneView.SwapChainFormat,
+                                                 usage: ImageUsageFlags.TransferDst | ImageUsageFlags.Sampled,
+                                                 name: "OverlayPanelGpuImage")
+            {
+                IsPreMultipliedAlpha = true,
+                HasTransparentPixels = true,
+            };
+        }
+
+
+        if (RenderedToSharedTexture)
+        {
+            // Copy the last rendered scene to _overlayPanelGpuImage
+            // This preserves the GpuImage on the GPU memory and is much faster than the option below where we render to RawImageData
+            _overlayPanelSceneView.SwapChain.CopyToGpuImage(_overlayPanelSceneView.RenderingContext.CurrentSwapChainImageIndex,
+                                                            targetGpuImage: _overlayPanelGpuImage, targetImageFinalLayout: ImageLayout.ShaderReadOnlyOptimal);
+        }
+        else
+        {
+            // Copy the rendered image to CPU memory (to RawImageData)
+            // This is much slower. It is show here to present this other option.
+
+            if (_rawImageData != null && (_rawImageData.Width != _overlayPanelSceneView.Width || _rawImageData.Height != _overlayPanelSceneView.Height))
+                _rawImageData = null;
+
+            if (_rawImageData == null)
+            {
+                _rawImageData = new RawImageData(width: _overlayPanelWidth,
+                                                 height: _overlayPanelHeight,
+                                                 stride: 4 * _overlayPanelWidth,
+                                                 format: _overlayPanelSceneView.SwapChainFormat,
+                                                 data: new byte[4 * _overlayPanelWidth * _overlayPanelHeight],
+                                                 checkTransparency: false)
+                {
+                    HasTransparentPixels = true
+                };
+            }
+
+            // Render Overlay 3D scene to _rawImageData
+            // NOTE that this copies the rendered texture to CPU memory.
+            _overlayPanelSceneView.RenderToRawImageData(_rawImageData);
+
+            // Copy the _rawImageData to a GpuImage that is rendered as a sprite
+            _overlayPanelGpuImage.CopyDataToImage(_rawImageData.Data, transitionImageToShaderReadOnlyOptimalLayout: true);
+        }
+
+
+        // Render _overlayPanelGpuImage as Sprite to the lower left corner
 
         _spriteBatch.Begin(useAbsoluteCoordinates: true);
         _spriteBatch.SetCoordinateCenter(PositionTypes.BottomLeft);
-        
-        _spriteBatch.SetSpriteTexture(_gpuImage);
+    
+        _spriteBatch.SetSpriteTexture(_overlayPanelGpuImage);
         _spriteBatch.DrawSprite(topLeftPosition: new Vector2(30, 286), spriteSize: new Vector2(256, 256));
-        
+    
         _spriteBatch.End();
     }
 }
