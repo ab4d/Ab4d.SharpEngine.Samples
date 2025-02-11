887d449912ec4c6717fc7944882ca1bd0433b869
abenedik <abenedik@ab4d.com>
2025-02-11 11:49:47 +0100

    Show Vulkan backend info icon also on Linux

--- Ab4d.SharpEngine.Samples.AvaloniaUI/SamplesWindow.axaml.cs
+++ Ab4d.SharpEngine.Samples.AvaloniaUI/SamplesWindow.axaml.cs
@@ -528,12 +528,10 @@ namespace Ab4d.SharpEngine.Samples.AvaloniaUI
                 _isSceneViewSizeChangedSubscribed = true;
             }
 
-            // If we are not using Vulkan backend on windows, then show info icon with tooltip about that.
-            // Vulkan backend can be also used on Linux, but only on X11 (how to check if we are running X11?)
-            VulkanBackendInfoImage.IsVisible = (sharpEngineSceneView is SharpEngineSceneView avaloniaSharpEngineSceneView && // IsVulkanBackend property is available only on SharpEngineSceneView from AvaloniaUI and not in the generic ISharpEngineSceneView
-                                                !avaloniaSharpEngineSceneView.IsVulkanBackend &&
-                                               avaloniaSharpEngineSceneView.PresentationType == PresentationTypes.SharedTexture &&
-                                                OperatingSystem.IsWindows());
+            // If we are not using Vulkan backend then show info icon with tooltip about that.
+            VulkanBackendInfoImage.IsVisible = sharpEngineSceneView is SharpEngineSceneView avaloniaSharpEngineSceneView && // IsVulkanBackend property is available only on SharpEngineSceneView from AvaloniaUI and not in the generic ISharpEngineSceneView
+                                               !avaloniaSharpEngineSceneView.IsVulkanBackend &&
+                                               avaloniaSharpEngineSceneView.PresentationType == PresentationTypes.SharedTexture;
 
             if (sharpEngineSceneView == null || !sharpEngineSceneView.SceneView.BackBuffersInitialized)
             {
