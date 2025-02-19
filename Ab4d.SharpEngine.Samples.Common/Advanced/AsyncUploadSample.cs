40027bd4967c7749daaffe7affa87635cc631693
Andrej Benedik <abenedik@ab4d.com>
2025-02-19 14:43:20 +0100

    Allowed using indexOffset and customIndexCount when using custom vertex or index buffers

--- Ab4d.SharpEngine.Samples.Common/Advanced/AsyncUploadSample.cs
+++ Ab4d.SharpEngine.Samples.Common/Advanced/AsyncUploadSample.cs
@@ -19,7 +19,6 @@ public class AsyncUploadSample : CommonSample
     private MeshModelNode? _meshModelNode1;
     private MeshModelNode? _meshModelNode2;
 
-    //private readonly DisposeList _disposables = new();
     private readonly string _textureFileName;
 
     private bool _isUploadDelayed = true;
@@ -28,13 +27,6 @@ public class AsyncUploadSample : CommonSample
         : base(context)
     {
         _textureFileName = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources/Textures/sharp-engine-logo.png");
-    }
-
-    /// <inheritdoc />
-    protected override void OnDisposed()
-    {
-        //_disposables.Dispose();
-        base.OnDisposed();
     }
 
     protected override void OnCreateScene(Scene scene)
@@ -221,11 +213,8 @@ public class AsyncUploadSample : CommonSample
         var vertices = newSphereMesh.Vertices!;
         var triangleIndices = newSphereMesh.TriangleIndices!;
 
-        var newVertexBuffer = await gpuDevice.CreateBufferAsync(vertices,        BufferUsageFlags.VertexBuffer, name: "LazyCreatedVertexBuffer");
-        var newIndexBuffer  = await gpuDevice.CreateBufferAsync(triangleIndices, BufferUsageFlags.IndexBuffer,  name: "LazyCreatedIndexBuffer");
-
-        //_disposables.Add(newVertexBuffer);
-        //_disposables.Add(newIndexBuffer);
+        var newVertexBuffer = await gpuDevice.CreateBufferAsync(vertices,        BufferUsageFlags.VertexBuffer, name: "AsyncCreatedVertexBuffer");
+        var newIndexBuffer  = await gpuDevice.CreateBufferAsync(triangleIndices, BufferUsageFlags.IndexBuffer,  name: "AsyncCreatedIndexBuffer");
 
         // We could also create a new GpuBuffer manually by using the GpuBuffer constructor and then call:
         //await newVertexBuffer.WriteToBufferAsync(vertices); // NOTE: The size of GpuBuffer must be big enough for the vertices
@@ -280,9 +269,6 @@ public class AsyncUploadSample : CommonSample
         var newVertexBuffer = gpuDevice.CreateBuffer(vertices,        usage: BufferUsageFlags.VertexBuffer, name: "VertexBuffer");
         var newIndexBuffer  = gpuDevice.CreateBuffer(triangleIndices, usage: BufferUsageFlags.IndexBuffer,  name: "IndexBuffer");
 
-        //_disposables.Add(newVertexBuffer);
-        //_disposables.Add(newIndexBuffer);
-
         // Set custom vertex and index buffer to MeshModelNode
         // See additional comments in CreateMeshAsync
         meshModelNode.SetCustomVertexBuffer(vertices, newVertexBuffer, newSphereMesh.BoundingBox);
@@ -292,6 +278,145 @@ public class AsyncUploadSample : CommonSample
         meshModelNode.Material = StandardMaterials.Green;
     }
 
+    private void AddManySceneNodes(int sceneNodesCount)
+    {
+        if (Scene == null || Scene.GpuDevice == null)
+            return;
+
+        float radius = 10;
+        Vector3 xOffset = new Vector3(radius * 1.2f, 0, 0);
+        Vector3 yOffset = new Vector3(0, radius * 1.2f, 0);
+        Vector3 offset = new Vector3(300, 0, -150);
+
+        int rowsCount = 10;
+
+        var greenMaterial = StandardMaterials.Green; // Use the same instance of StandardMaterial for all MeshModelNode; the StandardMaterials.Green getter creates a new instance on each call.
+
+        var groupNode = new GroupNode("AllSpheres");
+
+        for (int i = 0; i < sceneNodesCount; i++)
+        {
+            int x = i / rowsCount;
+            int y = i % rowsCount;
+
+            var oneSphereMesh = MeshFactory.CreateSphereMesh(offset + x * xOffset + y * yOffset, radius: 5, segments: 30);
+
+            // NOTE:
+            // Creating many sphere meshed with the same segments count just to have different position is very inefficient.
+            // Instead, curate a single CreateSphereMesh and the then use transformation to change the position or size of the sphere.
+            // But for this sample, we simulate a scenario where we would create many different meshes.
+
+            var oneMeshModelNode = new MeshModelNode(oneSphereMesh, greenMaterial);
+
+            groupNode.Add(oneMeshModelNode);
+        }
+
+        Scene.RootNode.Add(groupNode);
+
+        if (_isUploadDelayed)
+            System.Threading.Thread.Sleep(500);
+    }
+    
+    private async Task AddManySceneNodesAsync(int sceneNodesCount)
+    {
+        if (Scene == null || Scene.GpuDevice == null)
+            return;
+
+        // Create a complex sphere mesh in the background thread
+
+        var triangleIndicesCounts = new int[sceneNodesCount];
+        var boundingBoxes = new BoundingBox[sceneNodesCount];
+
+        StandardMesh? combinedMesh = null;
+
+        await Task.Run(() =>
+        {
+            //
+            // The following code is run on background thread:
+            //
+
+            var allMeshes = new List<StandardMesh>(sceneNodesCount);
+
+            float radius = 10;
+            Vector3 xOffset = new Vector3(radius * 1.2f, 0, 0);
+            Vector3 yOffset = new Vector3(0, radius * 1.2f, 0);
+            Vector3 offset = new Vector3(300, 0, 150);
+
+            int rowsCount = 10;
+
+            for (int i = 0; i < sceneNodesCount; i++)
+            {
+                int x = i / rowsCount;
+                int y = i % rowsCount;
+
+                var oneSphereMesh = MeshFactory.CreateSphereMesh(offset + x * xOffset + y * yOffset, radius: 5, segments: 30);
+                allMeshes.Add(oneSphereMesh);
+
+                // Save how many triangle indices is needed for this mesh so we can later get the offsets and indices count for each MeshModelNode
+                // Here we could use a single indices count because all sphere meshes have the same count of segments, but in reality we may use different meshes
+                triangleIndicesCounts[i] = (int)oneSphereMesh.IndexCount;
+
+                // We also need to save the individual bounding boxes
+                boundingBoxes[i] = oneSphereMesh.BoundingBox;
+            }
+
+            // Combine all meshes into a single mesh
+            combinedMesh = Ab4d.SharpEngine.Utilities.MeshUtils.CombineMeshes(allMeshes);
+
+            if (_isUploadDelayed)
+                System.Threading.Thread.Sleep(500);
+        });
+
+
+        if (combinedMesh == null)
+            return;
+
+        var gpuDevice = Scene.GpuDevice;
+        var combinedVertices = combinedMesh.Vertices!;
+        var combinedTriangleIndices = combinedMesh.TriangleIndices!;
+
+        //
+        // The data for vertex and index buffers are uploaded in the background thread
+        //
+        var combinedVertexBuffer = await gpuDevice.CreateBufferAsync(combinedVertices,        BufferUsageFlags.VertexBuffer, name: "AsyncCreatedCombinedVertexBuffer");
+        var combinedIndexBuffer  = await gpuDevice.CreateBufferAsync(combinedTriangleIndices, BufferUsageFlags.IndexBuffer,  name: "AsyncCreatedCombinedIndexBuffer");
+
+        // We could also create a new GpuBuffer manually by using the GpuBuffer constructor and then call:
+        //await newVertexBuffer.WriteToBufferAsync(vertices); // NOTE: The size of GpuBuffer must be big enough for the vertices
+
+        //
+        // The following code is running on UI thread (we must not create SceneNodes and Materials on background thread).
+        // NOTE: If this code still takes too long to run, then split adding new SceneNode objects into multiple frames,
+        //       for example, instead of adding 20,000 MeshModelNode in one frame, add 2000 per frame,
+        //       slightly reducing the frame rate for 10 frames. This can look much nicer for the user.
+        //
+        int indexOffset = 0;
+
+        var vertexBufferDescription = gpuDevice.VertexBufferDescriptionsManager.GetPositionNormalTextureVertexBufferDescription();
+        var greenMaterial = StandardMaterials.Green; // Use the same instance of StandardMaterial for all MeshModelNode; the StandardMaterials.Green getter creates a new instance on each call.
+
+        var groupNode = new GroupNode("AllAsyncSpheres");
+
+
+        for (int i = 0; i < sceneNodesCount; i++)
+        {
+            // First create a new StandardMesh with combined vertices and triangle indices - those are needed for hit-testing
+            var oneMesh = new StandardMesh(combinedVertices, combinedTriangleIndices, boundingBoxes[i], $"Mesh_{i}");
+
+            // The set custom vertex and index buffers and specify indexOffset and customIndexCount to use only part of the triangles
+            oneMesh.SetCustomVertexBuffer(combinedVertices, combinedVertexBuffer, boundingBoxes[i], vertexBufferDescription);
+            oneMesh.SetCustomIndexBuffer(combinedTriangleIndices, combinedIndexBuffer, indexOffset: indexOffset, customIndexCount: triangleIndicesCounts[i]);
+
+            // Then we can create a new MeshModelNode from the mesh
+            var oneMeshModelNode = new MeshModelNode(oneMesh, greenMaterial, name: $"MeshModelNode_{i}");
+
+            groupNode.Add(oneMeshModelNode);
+
+            indexOffset += triangleIndicesCounts[i];
+        }
+
+        Scene.RootNode.Add(groupNode);
+    }
 
     public class SlowStream : Stream
     {
@@ -351,7 +476,6 @@ public class AsyncUploadSample : CommonSample
     }
 
 
-
     /// <inheritdoc />
     protected override void OnCreateUI(ICommonSampleUIProvider ui)
     {
@@ -364,10 +488,13 @@ public class AsyncUploadSample : CommonSample
         ui.CreateButton("Update mesh buffers sync", () => CreateMesh(_meshModelNode2));
         ui.CreateButton("Update mesh buffers async", () => _ = CreateMeshAsync(_meshModelNode1)); // Note: calling async method from sync context. Discarding the result Task to prevent warning (adding "_ = ...")
         ui.AddSeparator();
-
-        ui.CreateButton("Reset materials to gray", () => ResetMaterials());
+        
+        ui.CreateButton("Add many SceneNodes sync", () => AddManySceneNodes(sceneNodesCount: 1000));
+        ui.CreateButton("Add many SceneNodes async", () => _ = AddManySceneNodesAsync(sceneNodesCount: 1000)); // Note: calling async method from sync context. Discarding the result Task to prevent warning (adding "_ = ...")
         ui.AddSeparator();
 
+        ui.CreateButton("Reset materials to gray", ResetMaterials);
+        ui.AddSeparator();
         ui.CreateCheckBox("Simulate long upload (?):When checked then a delay is added when creating the mesh to simulate a complex mesh upload.", _isUploadDelayed, isChecked => _isUploadDelayed = isChecked);
 
         if (Scene != null && Scene.GpuDevice != null && 
