d94ec7f8fbeb8de7944c8815c3a6cc5a358469c9
Andrej Benedik <abenedik@ab4d.com>
2025-02-21 12:17:12 +0100

    Using common SharpEngineLicenseHelper to activate SharpEngine license

--- Ab4d.SharpEngine.Samples.AvaloniaUI.CrossPlatform/Desktop/Program.cs
+++ Ab4d.SharpEngine.Samples.AvaloniaUI.CrossPlatform/Desktop/Program.cs
@@ -16,13 +16,8 @@ namespace Ab4d.SharpEngine.Samples.AvaloniaUI.CrossPlatform.Desktop
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
         {
-            // Ab4d.SharpEngine Samples License can be used only for Ab4d.SharpEngine samples.
-            // To use Ab4d.SharpEngine in your project, get a license from ab4d.com/trial or ab4d.com/purchase 
-            // We need to call SetLicense in the entry assembly so we cannot move this call to CrossPlatform project.
-            Ab4d.SharpEngine.Licensing.SetLicense(licenseOwner: "AB4D",
-                                                  licenseType: "SamplesLicense",
-                                                  platforms: "All",
-                                                  license: "5B53-8A17-DAEB-5B03-3B90-DD5B-958B-CA4D-0B88-CE79-FBB4-6002-D9C9-19C2-AFF8-1662-B2B2");
+            // Ab4d.SharpEngine license must be activated from the entry assembly (otherwise an SDK license is needed).
+            SharpEngineLicenseHelper.ActivateLicense();
 
             return AppBuilder.Configure<App>()
                 .UsePlatformDetect()
---
+++ Ab4d.SharpEngine.Samples.AvaloniaUI.CrossPlatform/Shared/SharpEngineLicenseHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Ab4d.SharpEngine.Samples.AvaloniaUI.CrossPlatform;
+
+public static class SharpEngineLicenseHelper
+{
+    public static void ActivateLicense()
+    {
+        // NOTE:
+        // SetLicense method must be called from the entry assembly (otherwise an SDK license is needed).
+        // This class is called from each entry assembly so we have license written in a single location.
+
+        // Ab4d.SharpEngine Samples License can be used only for Ab4d.SharpEngine samples.
+        // To use Ab4d.SharpEngine in your project, get a license from ab4d.com/trial or ab4d.com/purchase 
+        // We need to call SetLicense in the entry assembly so we cannot move this call to CrossPlatform project.
+        Ab4d.SharpEngine.Licensing.SetLicense(licenseOwner: "AB4D",
+                                              licenseType: "SamplesLicense",
+                                              platforms: "All",
+                                              license: "5B53-8A17-DAEB-5B03-3B90-DD5B-958B-CA4D-0B88-CE79-FBB4-6002-D9C9-19C2-AFF8-1662-B2B2");
+    }
+}
