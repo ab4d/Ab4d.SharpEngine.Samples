2e7bf3ac2472dfe5fb35ca8a176b2a6bda5ec5bd
Andrej Benedik <abenedik@ab4d.com>
2025-07-07 09:38:34 +0200

    Removed platforms parameter from SetLicense call This parameter is not required anymore in v3.1

--- Ab4d.SharpEngine.Samples.AvaloniaUI.CrossPlatform/Shared/Views/MainView.axaml.cs
+++ Ab4d.SharpEngine.Samples.AvaloniaUI.CrossPlatform/Shared/Views/MainView.axaml.cs
@@ -33,7 +33,6 @@ namespace Ab4d.SharpEngine.Samples.AvaloniaUI.CrossPlatform.Views
             //// To use Ab4d.SharpEngine in your project, get a license from ab4d.com/trial or ab4d.com/purchase 
             //Ab4d.SharpEngine.Licensing.SetLicense(licenseOwner: "AB4D",
             //                                      licenseType: "SamplesLicense",
-            //                                      platforms: "All",
             //                                      license: "5B53-8A17-DAEB-5B03-3B90-DD5B-958B-CA4D-0B88-CE79-FBB4-6002-D9C9-19C2-AFF8-1662-B2B2");
 
 
