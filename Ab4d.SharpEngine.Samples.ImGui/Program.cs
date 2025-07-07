450adaa3259fef44075cc1fdf37b5c41522cf187
Andrej Benedik <abenedik@ab4d.com>
2025-07-07 12:17:12 +0200

    Use SetLicense with 3 parameters

--- Ab4d.SharpEngine.Samples.ImGui/Program.cs
+++ Ab4d.SharpEngine.Samples.ImGui/Program.cs
@@ -47,7 +47,6 @@ internal class Program
         // To use Ab4d.SharpEngine in your project, get a license from ab4d.com/trial or ab4d.com/purchase
         Licensing.SetLicense(licenseOwner: "AB4D",
                              licenseType: "SamplesLicense",
-                             platforms: "All",
                              license: "5B53-8A17-DAEB-5B03-3B90-DD5B-958B-CA4D-0B88-CE79-FBB4-6002-D9C9-19C2-AFF8-1662-B2B2");
 
         // Enable logging of warnings and errors from SharpEngine. In debug builds, also enable Vulkan validation.
