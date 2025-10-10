d5ab1aa266a3ac3fe62c41c714f715d2e1a87008
Andrej Benedik <abenedik@ab4d.com>
2025-10-10 12:19:21 +0200

    Added commented code that shows how to call SetLicense

--- Ab4d.SharpEngine.Samples.Wpf/QuickStart/SharpEngineSceneViewInXaml.xaml.cs
+++ Ab4d.SharpEngine.Samples.Wpf/QuickStart/SharpEngineSceneViewInXaml.xaml.cs
@@ -35,6 +35,13 @@ namespace Ab4d.SharpEngine.Samples.Wpf.QuickStart
 
         public SharpEngineSceneViewInXaml()
         {
+            // Set the Ab4d.SharpEngine license before the engine is initialized.
+            // In this samples project, the SetLicense method is already called from the SamplesWindow.xaml.cs so this call is commented.
+            // To use the Ab4d.SharpEngine in your project, get a license from ab4d.com/trial or ab4d.com/purchase 
+            //Ab4d.SharpEngine.Licensing.SetLicense(licenseOwner: "AB4D",
+            //                                      licenseType: "SamplesLicense",
+            //                                      license: "5B53-8A17-DAEB-5B03-3B90-DD5B-958B-CA4D-0B88-CE79-FBB4-6002-D9C9-19C2-AFF8-1662-B2B2");            
+
             InitializeComponent();
             
             // This sample shows how to create SharpEngineSceneView in XAML.
