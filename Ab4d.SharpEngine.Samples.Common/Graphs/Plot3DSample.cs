916c0247e8f5b6bdc0b9f3f45efaf5f2dc7c5206
Andrej Benedik <abenedik@ab4d.com>
2026-03-16 13:25:28 +0100

    Using single HeightMapContoursNode to also generate bottom contour lines

--- Ab4d.SharpEngine.Samples.Common/Graphs/Plot3DSample.cs
+++ Ab4d.SharpEngine.Samples.Common/Graphs/Plot3DSample.cs
@@ -14,12 +14,17 @@ public class Plot3DSample : CommonSample
     
     private HeightMapSurfaceNode? _heightMapSurfaceNode;
     private HeightMapContoursNode? _heightMapContoursNode;
-    private HeightMapContoursNode? _bottomContoursNode;
     private HeightMapWireframeNode? _heightMapWireframeNode;
+
+    //private HeightMapContoursNode? _bottomContoursNode;
+    private GroupNode? _bottomContoursNode;
 
     private AxesBoxNode? _axesBoxNode;
 
     private int _selectedFunctionIndex;
+    private bool _showBottomContours = true;
+    
+    private GradientStop[] _gradientStops;
 
     private enum HeightMapLinesTypes
     {
@@ -29,12 +34,19 @@ public class Plot3DSample : CommonSample
     }
     
     private HeightMapLinesTypes _selectedHeightMapLines = HeightMapLinesTypes.ContourLines;
-
+    
 
     public Plot3DSample(ICommonSamplesContext context)
         : base(context)
     {
-
+        _gradientStops = new GradientStop[]
+        {
+            new GradientStop(Colors.Red, 1.0f),
+            new GradientStop(Colors.Yellow, 0.75f),
+            new GradientStop(Colors.LightGreen, 0.5f),
+            new GradientStop(Colors.Aqua, 0.25f),
+            new GradientStop(Colors.Blue, 0.0f)
+        };
     }
 
     protected override async Task OnCreateSceneAsync(Scene scene)
@@ -53,21 +65,11 @@ public class Plot3DSample : CommonSample
 
         var textBlockFactory = await context.GetTextBlockFactoryAsync();
 
-        var gradientStops = new GradientStop[]
-        {
-            new GradientStop(Colors.Red, 1.0f),
-            new GradientStop(Colors.Yellow, 0.75f),
-            new GradientStop(Colors.LightGreen, 0.5f),
-            new GradientStop(Colors.Aqua, 0.25f),
-            new GradientStop(Colors.Blue, 0.0f)
-        };
-
-
         StandardMaterial? gradientMaterial;
 
         if (scene.GpuDevice != null)
         {
-            var gradientGpuImage = TextureFactory.CreateGradientTexture(scene.GpuDevice, gradientStops, textureSize: 256);
+            var gradientGpuImage = TextureFactory.CreateGradientTexture(scene.GpuDevice, _gradientStops, textureSize: 256);
             gradientMaterial = new StandardMaterial(gradientGpuImage, CommonSamplerTypes.Clamp, name: $"GradientTexture");
         }
         else
@@ -77,6 +79,7 @@ public class Plot3DSample : CommonSample
 
         var data = GetFunctionData(selectedFunctionIndex: 0);
         
+
         _heightMapSurfaceNode = new HeightMapSurfaceNode(centerPosition: new Vector3(0, 10, 0),
                                                          size: new Vector3(100, 20, 100),
                                                          heightData: data,
@@ -97,33 +100,42 @@ public class Plot3DSample : CommonSample
                                                            numContourLines: 14,
                                                            majorLinesFrequency: 0,
                                                            verticalOffset: 0.05f, // lift the grid slightly on top of the HeightMap
-                                                           combineContourLines: true, // all contour lines will have the same color and thickness, so we can draw them with one line
+                                                           combineContourLines: false, // even though all the contour lines are shown with the same color, we must not combine them, because we will use the data from individual lines to create the contour lines at the bottom of the graph
                                                            name: "HeightMapContoursNode");
 
         scene.RootNode.Add(_heightMapContoursNode);
 
 
-        _bottomContoursNode = new HeightMapContoursNode(_heightMapSurfaceNode,
-                                                        numContourLines: 14,
-                                                        majorLinesFrequency: 0,
-                                                        combineContourLines: false, // Each contour line will have its own color so we must not combine them
-                                                        name: "HeightMapContoursNode")
-        {
-            // Setting LineThickness and LineColor does not call UpdateMesh
-            MinorLineThickness = 1f,
-            MajorLineThickness = 2f,
-        };
+        // The following commented code shows how to create flatten and colored contour lines
+        // creating a new HeightMapContoursNode object. This is easier to be done, but it not as efficient as using one HeightMapContoursNode 
+        // and using its calculated contour lines for the lines on the graph and at bottom of the graph.
 
-        // Color each contour line based on its height. This will create a gradient colored contour lines on the bottom of the graph.
-        _bottomContoursNode.GetCustomLineColorCallback = contourHeight => TextureFactory.GetGradientColor(contourHeight, gradientStops);
+        //_bottomContoursNode = new HeightMapContoursNode(_heightMapSurfaceNode,
+        //                                                numContourLines: 14,
+        //                                                majorLinesFrequency: 0,
+        //                                                combineContourLines: false, // Each contour line will have its own color so we must not combine them
+        //                                                name: "HeightMapContoursNode")
+        //{
+        //    // Setting LineThickness and LineColor does not call UpdateMesh
+        //    MinorLineThickness = 1f,
+        //    MajorLineThickness = 2f,
+        //};
 
-        // Flatten all the lines by setting the same Y value (-40) for all positions in the contour lines.
-        // This will show colored contour lines on the bottom of the graph.
-        _bottomContoursNode.FlattenContours(fixedHeightValue: -40);
+        //// Color each contour line based on its height. This will create a gradient colored contour lines on the bottom of the graph.
+        //_bottomContoursNode.GetCustomLineColorCallback = contourHeight => TextureFactory.GetGradientColor(contourHeight, _gradientStops);
+
+        //// Flatten all the lines by setting the same Y value (-40) for all positions in the contour lines.
+        //// This will show colored contour lines on the bottom of the graph.
+        //_bottomContoursNode.FlattenContours(fixedHeightValue: -40);
+
+
+        // Instead of new HeightMapContoursNode we will group the lines in a GroupNode
+
+        _bottomContoursNode = new GroupNode("BottomContoursNode");
 
         scene.RootNode.Add(_bottomContoursNode);
-        
-        
+
+
         // Create height map wireframe, and tie its properties to the height map surface.
         // Set all available parameters in the constructor, because changing those values later will call UpdateMesh on each change.
         _heightMapWireframeNode = new HeightMapWireframeNode(_heightMapSurfaceNode, 
@@ -134,7 +146,7 @@ public class Plot3DSample : CommonSample
         {
             // Changing LineColor and Visibility will not call UpdateMesh
             LineColor = Colors.Black,
-            Visibility = SceneNodeVisibility.Hidden,
+            Visibility = SceneNodeVisibility.Hidden, // Initially hidden
         };
         
         scene.RootNode.Add(_heightMapWireframeNode);
@@ -142,7 +154,7 @@ public class Plot3DSample : CommonSample
         
         _axesBoxNode = new AxesBoxNode(textBlockFactory.BitmapTextCreator)
         {
-            CenterPosition = new Vector3(0, 50, 0),
+            CenterPosition = new Vector3(0, 0, 0),
             Size = new Vector3(100, 80, 100),
 
             // Show only Z axis
@@ -156,6 +168,7 @@ public class Plot3DSample : CommonSample
         
         _axesBoxNode.SetAxisDataRange(AxesBoxNode.AxisTypes.XAxis, minimumValue: 0, maximumValue: _arraySize, majorTicksStep: 1, minorTicksStep: 0, snapMaximumValueToMajorTicks: false);
         _axesBoxNode.SetAxisDataRange(AxesBoxNode.AxisTypes.YAxis, minimumValue: 0, maximumValue: _arraySize, majorTicksStep: 1, minorTicksStep: 0, snapMaximumValueToMajorTicks: false);
+        _axesBoxNode.SetAxisDataRange(AxesBoxNode.AxisTypes.ZAxis, minimumValue: -2, maximumValue: 2, majorTicksStep: 1, minorTicksStep: 0, snapMaximumValueToMajorTicks: false);
         
         scene.RootNode.Add(_axesBoxNode);
         
@@ -193,6 +206,8 @@ public class Plot3DSample : CommonSample
         if (_heightMapSurfaceNode != null)
             _heightMapSurfaceNode.HeightData = GetFunctionData(selectedFunctionIndex);
 
+        UpdateBottomContourLines();
+
         _selectedFunctionIndex = selectedFunctionIndex;
     }
 
@@ -203,6 +218,55 @@ public class Plot3DSample : CommonSample
         
         if (_heightMapWireframeNode != null)
             _heightMapWireframeNode.Visibility = _selectedHeightMapLines == HeightMapLinesTypes.Wireframe ? SceneNodeVisibility.Visible : SceneNodeVisibility.Hidden;
+    }
+
+    private void UpdateBottomContourLines()
+    {
+        if (_bottomContoursNode != null)
+        {
+            _bottomContoursNode.Visibility = _showBottomContours && _selectedHeightMapLines == HeightMapLinesTypes.ContourLines ? SceneNodeVisibility.Visible
+                                                                                                                                : SceneNodeVisibility.Hidden;
+        }
+
+
+        if (_selectedHeightMapLines != HeightMapLinesTypes.ContourLines || _heightMapContoursNode == null || _bottomContoursNode == null)
+            return;
+
+        _bottomContoursNode.Clear();
+
+        var allContourLineHeights = _heightMapContoursNode.GetAvailableLineHeights();
+
+        if (allContourLineHeights == null)
+        {
+            _heightMapContoursNode.Update();
+            allContourLineHeights = _heightMapContoursNode.GetAvailableLineHeights();
+        }
+
+        if (allContourLineHeights != null)
+        {
+            var bottomYPosition = -40;
+
+            foreach (var contourLineHeight in allContourLineHeights)
+            {
+                var multiLineNode = _heightMapContoursNode.GetLineNode(contourLineHeight);
+                var positions = multiLineNode?.Positions;
+
+                if (positions != null)
+                {
+                    // Copy positions but set y value to bottomYPosition for all positions
+                    var bottomContourLinePositions = new Vector3[positions.Length];
+                    for (var i = 0; i < positions.Length; i++)
+                        bottomContourLinePositions[i] = new Vector3(positions[i].X, bottomYPosition, positions[i].Z);
+
+                    // Get color based on the height
+                    var color = TextureFactory.GetGradientColor(contourLineHeight, _gradientStops);
+
+                    // Create a new line
+                    var lineNode = new MultiLineNode(bottomContourLinePositions, isLineStrip: false, lineColor: color, lineThickness: 1.5f);
+                    _bottomContoursNode.Add(lineNode);
+                }
+            }
+        }
     }
 
     #region Math functions
@@ -358,26 +422,8 @@ public class Plot3DSample : CommonSample
     {
         ui.CreateStackPanel(PositionTypes.Bottom | PositionTypes.Right);
         
-        ui.CreateCheckBox("Show AxesBoxNode", true, isChecked => _axesBoxNode!.Visibility = isChecked ? SceneNodeVisibility.Visible : SceneNodeVisibility.Hidden);
-        
-        ui.CreateCheckBox("Show bottom contours", true, isChecked => _bottomContoursNode!.Visibility = isChecked ? SceneNodeVisibility.Visible : SceneNodeVisibility.Hidden);
-        
-        ui.AddSeparator();
-        
-        
-        ui.CreateRadioButtons(new string[] { "No value lines", "Contour lines", "Wireframe lines" },
-            (selectedIndex, selectedText) => 
-            {
-                _selectedHeightMapLines = (HeightMapLinesTypes)selectedIndex;
-                UpdateHeightMapLines();
-            },
-            selectedItemIndex: 1);
-             
-        ui.AddSeparator();
-        
-        
         ui.CreateLabel("Function:");
-        
+
         ui.CreateRadioButtons(new string[]
             {
                 "y(x,z) = cos(x*z) * (x*x - z*z)",
@@ -393,12 +439,36 @@ public class Plot3DSample : CommonSample
         ui.AddSeparator();
         
         
+        ui.CreateRadioButtons(new string[] { "No value lines", "Contour lines", "Wireframe lines" },
+            (selectedIndex, selectedText) => 
+            {
+                _selectedHeightMapLines = (HeightMapLinesTypes)selectedIndex;
+                UpdateHeightMapLines();
+                UpdateBottomContourLines();
+            },
+            selectedItemIndex: 1);
+             
+        ui.AddSeparator();
+        
+        ui.CreateLabel("Data array size:");
+        
         ui.CreateRadioButtons(new string[] { "40 x 40", "80 x 80", "160 x 160" },
             (selectedIndex, selectedText) => 
             {
                 _arraySize = new int[] { 40, 80, 160 }[selectedIndex];
                 UpdateAll(_selectedFunctionIndex);
             },
-            selectedItemIndex: 1);           
+            selectedItemIndex: 1);      
+        
+        ui.AddSeparator();
+        
+
+        ui.CreateCheckBox("Show AxesBoxNode", true, isChecked => _axesBoxNode!.Visibility = isChecked ? SceneNodeVisibility.Visible : SceneNodeVisibility.Hidden);
+        
+        ui.CreateCheckBox("Show bottom contours", _showBottomContours, isChecked =>
+        {
+            _showBottomContours = isChecked;
+            UpdateBottomContourLines();
+        });
     }
 }
